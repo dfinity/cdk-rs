b60107045075cee8bd376d68e2036f5ff1ccccdb
Hans Larsen <hans@larsen.online>
2020-06-10 09:54:03 -0700

    feat: add println and eprintln macros and remove warning

--- src/ic_cdk/src/lib.rs
+++ src/ic_cdk/src/lib.rs
@@ -13,10 +13,44 @@ pub use api::*;
 use std::sync::Once;
 static START: Once = Once::new();
 
+/// Setup the stdlib hooks.
 pub fn setup() -> () {
     START.call_once(|| printer::hook());
 }
 
+/// Block on a promise in a WASM-friendly way (no multithreading!).
 pub fn block_on<F: 'static + std::future::Future<Output = ()>>(future: F) -> () {
     futures::block_on(future);
+}
+
+/// Format and then print the formatted message
+#[cfg(target_arch = "wasm32")]
+#[macro_export]
+macro_rules! println {
+    ($fmt:expr) => (ic_cdk::print(format!($fmt)));
+    ($fmt:expr, $($arg:tt)*) => (ic_cdk::print(format!($fmt, $($arg)*)));
+}
+
+/// Format and then print the formatted message
+#[cfg(not(target_arch = "wasm32"))]
+#[macro_export]
+macro_rules! println {
+    ($fmt:expr) => (std::print!($fmt));
+    ($fmt:expr, $($arg:tt)*) => (std::print!($fmt, $($arg)*));
+}
+
+/// Format and then print the formatted message
+#[cfg(target_arch = "wasm32")]
+#[macro_export]
+macro_rules! eprintln {
+    ($fmt:expr) => (ic_cdk::print(format!($fmt)));
+    ($fmt:expr, $($arg:tt)*) => (ic_cdk::print(format!($fmt, $($arg)*)));
+}
+
+/// Format and then print the formatted message
+#[cfg(not(target_arch = "wasm32"))]
+#[macro_export]
+macro_rules! eprintln {
+    ($fmt:expr) => (std::eprintln!($fmt));
+    ($fmt:expr, $($arg:tt)*) => (std::eprintln!($fmt, $($arg)*));
 }
