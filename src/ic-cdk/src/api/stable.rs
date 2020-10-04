d5e52e771ec268284b072120e4835310aa5ef01e
Hans <hans@larsen.online>
2020-10-03 23:06:02 -0700

    feat: add stable memory support using Candid for serialization (#27)

---
+++ src/ic-cdk/src/api/stable.rs
@@ -0,0 +1,128 @@
+use std::io;
+
+pub fn stable_size() -> u32 {
+    unsafe { super::ic0::stable_size() as u32 }
+}
+
+/// Attempt to grow the stable memory by `new_pages` (added pages).
+/// Returns an error if it wasn't possible. Otherwise, returns the previous
+/// size that was reserved.
+///
+/// ## Notes
+/// Pages are 64KiB in WASM.
+pub fn stable_grow(new_pages: u32) -> Result<u32, ()> {
+    unsafe {
+        match super::ic0::stable_grow(new_pages as i32) {
+            -1 => Err(()),
+            x => Ok(x as u32),
+        }
+    }
+}
+
+pub fn stable_write(offset: u32, buf: &[u8]) {
+    unsafe {
+        super::ic0::stable_write(offset as i32, buf.as_ptr() as i32, buf.len() as i32);
+    }
+}
+
+pub fn stable_read(offset: u32, buf: &mut [u8]) {
+    unsafe {
+        super::ic0::stable_read(buf.as_ptr() as i32, offset as i32, buf.len() as i32);
+    }
+}
+
+/// Returns a copy of the stable memory. This will map the whole memory (even if not all of it
+/// has been written to).
+pub fn stable_bytes() -> Vec<u8> {
+    let size = (stable_size() as usize) << 16;
+    let mut vec = Vec::with_capacity(size);
+    unsafe {
+        vec.set_len(size);
+    }
+
+    stable_read(0, vec.as_mut_slice());
+
+    vec
+}
+
+/// A writer to the stable memory. Will attempt to grow the memory as it
+/// writes, and keep offsets and total capacity.
+pub struct StableWriter {
+    /// The offset of the next write.
+    offset: usize,
+
+    /// The capacity, in pages.
+    capacity: u32,
+}
+
+impl Default for StableWriter {
+    fn default() -> Self {
+        let capacity = stable_size();
+
+        Self {
+            offset: 0,
+            capacity,
+        }
+    }
+}
+
+impl StableWriter {
+    /// Attempt to grow the memory by adding new pages.
+    pub fn grow(&mut self, added_pages: u32) -> Result<(), ()> {
+        let old_page_count = stable_grow(added_pages)?;
+        self.capacity = old_page_count + added_pages;
+        Ok(())
+    }
+
+    /// Write a byte slice to the buffer. The only condition where this will
+    /// error out is if it cannot grow the memory.
+    pub fn write(&mut self, buf: &[u8]) -> Result<usize, ()> {
+        if self.offset + buf.len() > ((self.capacity as usize) << 16) {
+            self.grow((buf.len() >> 16) as u32 + 1)?;
+        }
+
+        stable_write(self.offset as u32, buf);
+        self.offset += buf.len();
+        Ok(buf.len())
+    }
+}
+
+impl io::Write for StableWriter {
+    fn write(&mut self, buf: &[u8]) -> Result<usize, io::Error> {
+        self.write(buf)
+            .map_err(|_| io::Error::new(io::ErrorKind::Other, "Out Of Memory"))
+    }
+
+    fn flush(&mut self) -> Result<(), io::Error> {
+        // Noop.
+        Ok(())
+    }
+}
+
+/// A reader to the stable memory. Keeps an offset and reads off stable memory
+/// consecutively.
+pub struct StableReader {
+    /// The offset of the next write.
+    offset: usize,
+}
+
+impl Default for StableReader {
+    fn default() -> Self {
+        Self { offset: 0 }
+    }
+}
+
+impl StableReader {
+    pub fn read(&mut self, buf: &mut [u8]) -> Result<usize, ()> {
+        stable_read(self.offset as u32, buf);
+        self.offset += buf.len();
+        Ok(buf.len())
+    }
+}
+
+impl io::Read for StableReader {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize, io::Error> {
+        self.read(buf)
+            .map_err(|_| io::Error::new(io::ErrorKind::Other, "Unexpected error."))
+    }
+}
--- src/ic-cdk/src/storage.rs
+++ src/ic-cdk/src/storage.rs
@@ -1,3 +1,4 @@
+use crate::api::stable;
 use std::any::{Any, TypeId};
 use std::collections::BTreeMap;
 
@@ -36,4 +37,30 @@ pub fn get_mut<T: Sized + Default + 'static>() -> &'static mut T {
 
 pub fn get<T: Sized + Default + 'static>() -> &'static T {
     get_mut::<T>()
+}
+
+/// Save the storage to the stable memory, from storage.
+/// This will override any value previously stored to stable memory.
+pub fn stable_save<T>(t: T) -> Result<(), candid::Error>
+where
+    T: candid::ser::ArgumentEncoder,
+{
+    candid::ser::write_args(&mut stable::StableWriter::default(), t)
+}
+
+/// Restore a value to the storage, from the stable memory.
+/// There can only be one value in stable memory, currently.
+pub fn stable_restore<T>() -> Result<T, String>
+where
+    T: for<'de> candid::de::ArgumentDecoder<'de>,
+{
+    let bytes = stable::stable_bytes();
+
+    let mut de =
+        candid::de::IDLDeserialize::new(bytes.as_slice()).map_err(|e| format!("{:?}", e))?;
+    let res = candid::de::ArgumentDecoder::decode(&mut de).map_err(|e| format!("{:?}", e))?;
+    // The idea here is to ignore an error that comes from Candid, because we have trailing
+    // bytes.
+    let _ = de.done();
+    Ok(res)
 }
