f708b446361a4968598ea0055c13c477a97f2be9
lsgunnlsgunn <lisa.s.gunn@gmail.com>
2020-10-09 14:01:56 -0700

    Remove reflection-2 lines

--- docs/modules/rust-guide/examples/profile.rs
+++ docs/modules/rust-guide/examples/profile.rs
@@ -17,7 +17,7 @@ struct Profile {
 
 #[query(name = "getSelf")]
 fn get_self() -> Profile {
-    let id = ic-cdk::reflection::caller();
+    let id = ic-cdk::caller();
     let profile_store = storage::get::<ProfileStore>();
 
     profile_store
@@ -39,7 +39,7 @@ fn get(name: String) -> Profile {
 
 #[update]
 fn update(profile: Profile) {
-    let principal_id = ic-cdk::reflection::caller();
+    let principal_id = ic-cdk::caller();
     let id_store = storage::get_mut::<IdStore>();
     let profile_store = storage::get_mut::<ProfileStore>();
 
