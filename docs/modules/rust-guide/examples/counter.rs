67de5a9fd9fa581bbe2bfc93867e9750663dfd2f
lsgunnlsgunn <lisa.s.gunn@gmail.com>
2020-10-13 15:59:53 -0700

    Simplify counter.rs round 1

--- docs/modules/rust-guide/examples/counter.rs
+++ docs/modules/rust-guide/examples/counter.rs
@@ -1,13 +1,10 @@
 use ic_cdk_macros::*;
-use ic_types::Principal;
 
 static mut COUNTER: Option<candid::Nat> = None;
-static mut OWNER: Option<Principal> = None;
 
 #[init]
 fn init() {
     unsafe {
-        OWNER = Some(ic_cdk::reflection::caller());
         COUNTER = Some(candid::Nat::from(0));
     }
 }
@@ -15,7 +12,6 @@ fn init() {
 #[update]
 fn increment() -> () {
     unsafe {
-        ic_cdk::println!("{:?}", OWNER);
         COUNTER.as_mut().unwrap().0 += 1u64;
     }
 }
