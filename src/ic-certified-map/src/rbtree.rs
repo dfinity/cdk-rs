b0d3e028eb7765c53e3e8a207e72994cf8dfbade
chenyan-dfinity <yan.chen@dfinity.org>
2021-06-25 20:19:03 -0700

    lint

--- src/ic-certified-map/src/rbtree.rs
+++ src/ic-certified-map/src/rbtree.rs
@@ -657,11 +657,10 @@ impl<K: 'static + AsRef<[u8]>, V: AsHashTree + 'static> RbTree<K, V> {
                     std::mem::swap(&mut (*h).key, &mut (*m).key);
                     std::mem::swap(&mut (*h).value, &mut (*m).value);
                     (*h).right = delete_min((*h).right);
-                    (*h).subtree_hash = Node::subtree_hash(h);
                 } else {
                     (*h).right = go((*h).right, key);
-                    (*h).subtree_hash = Node::subtree_hash(h);
                 }
+                (*h).subtree_hash = Node::subtree_hash(h);
             }
             balance(h)
         }
