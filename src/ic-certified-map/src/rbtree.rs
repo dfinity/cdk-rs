15158b5cfc40ffb50108a191ab878a60170781fd
Roman Kashitsyn <roman@dfinity.org>
2021-09-16 12:55:56 +0200

    apply review feedback

--- src/ic-certified-map/src/rbtree.rs
+++ src/ic-certified-map/src/rbtree.rs
@@ -107,8 +107,8 @@ impl<K: 'static + AsRef<[u8]>, V: AsHashTree + 'static> Node<K, V> {
         Box::new(Self {
             key,
             value,
-            left: Node::null(),
-            right: Node::null(),
+            left: None,
+            right: None,
             color: Color::Red,
             subtree_hash: data_hash,
         })
@@ -130,10 +130,6 @@ impl<K: 'static + AsRef<[u8]>, V: AsHashTree + 'static> Node<K, V> {
             None => Empty,
             Some(r) => Pruned(r.subtree_hash),
         }
-    }
-
-    fn null() -> NodeRef<K, V> {
-        None
     }
 
     fn visit<'a, F>(n: &'a NodeRef<K, V>, f: &mut F)
@@ -204,7 +200,7 @@ impl<K: 'static + AsRef<[u8]>, V: AsHashTree + 'static> Default for RbTree<K, V>
 impl<K: 'static + AsRef<[u8]>, V: AsHashTree + 'static> RbTree<K, V> {
     /// Constructs a new empty tree.
     pub fn new() -> Self {
-        Self { root: Node::null() }
+        Self { root: None }
     }
 
     /// Returns true if the map is empty.
@@ -654,7 +650,7 @@ impl<K: 'static + AsRef<[u8]>, V: AsHashTree + 'static> RbTree<K, V> {
             if h.left.is_none() {
                 debug_assert!(h.right.is_none());
                 drop(h);
-                return Node::null();
+                return None;
             }
             if !is_red(&h.left) && !is_red(&h.left.as_ref().unwrap().left) {
                 h = move_red_left(h);
@@ -669,6 +665,7 @@ impl<K: 'static + AsRef<[u8]>, V: AsHashTree + 'static> RbTree<K, V> {
             key: &[u8],
         ) -> NodeRef<K, V> {
             if key < h.key.as_ref() {
+                debug_assert!(h.left.is_some(), "the key must be present in the tree");
                 if !is_red(&h.left) && !is_red(&h.left.as_ref().unwrap().left) {
                     h = move_red_left(h);
                 }
@@ -680,7 +677,7 @@ impl<K: 'static + AsRef<[u8]>, V: AsHashTree + 'static> RbTree<K, V> {
                 if key == h.key.as_ref() && h.right.is_none() {
                     debug_assert!(h.left.is_none());
                     drop(h);
-                    return Node::null();
+                    return None;
                 }
 
                 if !is_red(&h.right) && !is_red(&h.right.as_ref().unwrap().left) {
