d20c58d22354bda366a0bd5e4e2a3ffa6aa3f494
Linwei Shang <linwei.shang@dfinity.org>
2021-10-19 16:24:57 -0700

    fmt

--- src/ic-cdk-macros/tests/compile_fail/lifecycle_functions_should_have_no_return.rs
+++ src/ic-cdk-macros/tests/compile_fail/lifecycle_functions_should_have_no_return.rs
@@ -4,15 +4,15 @@ use ic_cdk_macros::*;
 fn init() -> u32 {}
 
 #[pre_upgrade]
-fn pre_upgrade()  -> u32 {}
+fn pre_upgrade() -> u32 {}
 
 #[post_upgrade]
-fn post_upgrade()  -> u32 {}
+fn post_upgrade() -> u32 {}
 
 #[heartbeat]
-fn heartbeat()  -> u32 {}
+fn heartbeat() -> u32 {}
 
 #[inspect_message]
-fn inspect_message()  -> u32 {}
+fn inspect_message() -> u32 {}
 
 fn main() {}
