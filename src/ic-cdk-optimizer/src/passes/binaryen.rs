b24a8b278cbca04314fa5fcad8df579590356198
adamspofford-dfinity <93943719+adamspofford-dfinity@users.noreply.github.com>
2022-02-17 12:58:55 -0800

    chore: Update clap to 3.1 (#209)
    * Update clap to 3.1
    
    * Remove conflicting arguments
    
    None of this appears to serve any purpose

--- src/ic-cdk-optimizer/src/passes/binaryen.rs
+++ src/ic-cdk-optimizer/src/passes/binaryen.rs
@@ -1,19 +1,9 @@
 use crate::passes::{OptimizationPass, PassResult};
 use binaryen::{CodegenConfig, Module};
-use clap::{ArgMatches, Clap, IntoApp};
 
-pub struct BinaryenPass {}
-
-impl BinaryenPass {}
-
-#[derive(Clap, Debug)]
-pub struct BinaryenPassArgs {}
+pub struct BinaryenPass;
 
 impl OptimizationPass for BinaryenPass {
-    fn args(&self) -> clap::App {
-        BinaryenPassArgs::into_app()
-    }
-
     fn short_name(&self) -> String {
         String::from("binaryen")
     }
@@ -22,7 +12,7 @@ impl OptimizationPass for BinaryenPass {
         String::from("Execute a binaryen optimization pass on your WASM.")
     }
 
-    fn opt(&self, wasm: &[u8], _matches: &ArgMatches) -> PassResult {
+    fn opt(&self, wasm: &[u8]) -> PassResult {
         let mut module =
             Module::read(wasm).map_err(|_| String::from("Could not load module..."))?;
 
