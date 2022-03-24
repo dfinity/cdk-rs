4147564ebb435f5e479e2201fbd7614209d154c3
Nicolas Mattia <nicolas@nmattia.com>
2022-03-24 16:52:06 +0100

    docs: Add example of using init args (#234)
    This adds an example and quick explanation of how arguments are dealt
    with in the `init` macro.

--- src/ic-cdk-macros/src/lib.rs
+++ src/ic-cdk-macros/src/lib.rs
@@ -228,6 +228,27 @@ pub fn update(attr: TokenStream, item: TokenStream) -> TokenStream {
 /// # unimplemented!()
 /// }
 /// ```
+///
+/// The init function may accept an argument, if that argument is a `CandidType`:
+///
+/// ```rust
+/// # use ic_cdk_macros::*;
+///
+/// #[derive(Clone, Debug, CandidType, Deserialize)]
+/// struct InitArg {
+///     foo: u8,
+/// }
+///
+/// #[init]
+/// fn init_function(arg: InitArg) {
+///     // ...
+/// # unimplemented!()
+/// }
+/// ```
+///
+/// In this case, the argument will be read from `ic0.msg_arg_data_size/copy` and passed to the
+/// init function upon successful deserialization.
+/// Refer to the [`canister_init` Specification](https://smartcontracts.org/docs/interface-spec/index.html#system-api-init) for more information.
 #[proc_macro_attribute]
 pub fn init(attr: TokenStream, item: TokenStream) -> TokenStream {
     handle_debug_and_errors(export::ic_init, "ic_init", attr, item)
