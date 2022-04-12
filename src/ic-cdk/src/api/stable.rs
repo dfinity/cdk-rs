43044af95a678e98bc70bdbb81b0ab49e35db728
Hamish Peebles <hamishpeebles@gmail.com>
2022-04-13 00:42:24 +0100

    feat: Add `BufferedStableWriter` which aims to minimise system calls (#245)

--- src/ic-cdk/src/api/stable.rs
+++ src/ic-cdk/src/api/stable.rs
@@ -2,16 +2,60 @@
 //!
 //! You can check the [Internet Computer Specification](https://smartcontracts.org/docs/interface-spec/index.html#system-api-stable-memory)
 //! for a in-depth explanation of stable memory.
+mod canister;
+#[cfg(test)]
+mod tests;
+
+use canister::CanisterStableMemory;
 use std::{error, fmt, io};
 
-/// Gets current size of the stable memory.
+const WASM_PAGE_SIZE_IN_BYTES: usize = 64 * 1024; // 64KB
+
+static CANISTER_STABLE_MEMORY: CanisterStableMemory = CanisterStableMemory {};
+
+/// A trait defining the stable memory API which each canister running on the IC can make use of
+pub trait StableMemory {
+    /// Gets current size of the stable memory (in WASM pages).
+    fn stable_size(&self) -> u32;
+
+    /// Similar to `stable_size` but with support for 64-bit addressed memory.
+    fn stable64_size(&self) -> u64;
+
+    /// Attempts to grow the stable memory by `new_pages` (added pages).
+    ///
+    /// Returns an error if it wasn't possible. Otherwise, returns the previous
+    /// size that was reserved.
+    ///
+    /// *Note*: Pages are 64KiB in WASM.
+    fn stable_grow(&self, new_pages: u32) -> Result<u32, StableMemoryError>;
+
+    /// Similar to `stable_grow` but with support for 64-bit addressed memory.
+    fn stable64_grow(&self, new_pages: u64) -> Result<u64, StableMemoryError>;
+
+    /// Writes data to the stable memory location specified by an offset.
+    ///
+    /// Warning - this will panic if `offset + buf.len()` exceeds the current size of stable memory.
+    /// Use `stable_grow` to request more stable memory if needed.
+    fn stable_write(&self, offset: u32, buf: &[u8]);
+
+    /// Similar to `stable_write` but with support for 64-bit addressed memory.
+    fn stable64_write(&self, offset: u64, buf: &[u8]);
+
+    /// Reads data from the stable memory location specified by an offset.
+    fn stable_read(&self, offset: u32, buf: &mut [u8]);
+
+    /// Similar to `stable_read` but with support for 64-bit addressed memory.
+    fn stable64_read(&self, offset: u64, buf: &mut [u8]);
+}
+
+/// Gets current size of the stable memory (in WASM pages).
 pub fn stable_size() -> u32 {
-    unsafe { super::ic0::stable_size() as u32 }
+    CANISTER_STABLE_MEMORY.stable_size()
 }
 
 /// Similar to `stable_size` but with support for 64-bit addressed memory.
 pub fn stable64_size() -> u64 {
-    unsafe { super::ic0::stable64_size() as u64 }
+    CANISTER_STABLE_MEMORY.stable64_size()
 }
 
 /// A possible error value when dealing with stable memory.
@@ -41,22 +85,12 @@ impl error::Error for StableMemoryError {}
 ///
 /// *Note*: Pages are 64KiB in WASM.
 pub fn stable_grow(new_pages: u32) -> Result<u32, StableMemoryError> {
-    unsafe {
-        match super::ic0::stable_grow(new_pages as i32) {
-            -1 => Err(StableMemoryError::OutOfMemory),
-            x => Ok(x as u32),
-        }
-    }
+    CANISTER_STABLE_MEMORY.stable_grow(new_pages)
 }
 
 /// Similar to `stable_grow` but with support for 64-bit addressed memory.
 pub fn stable64_grow(new_pages: u64) -> Result<u64, StableMemoryError> {
-    unsafe {
-        match super::ic0::stable64_grow(new_pages as i64) {
-            -1 => Err(StableMemoryError::OutOfMemory),
-            x => Ok(x as u64),
-        }
-    }
+    CANISTER_STABLE_MEMORY.stable64_grow(new_pages)
 }
 
 /// Writes data to the stable memory location specified by an offset.
@@ -64,30 +98,22 @@ pub fn stable64_grow(new_pages: u64) -> Result<u64, StableMemoryError> {
 /// Warning - this will panic if `offset + buf.len()` exceeds the current size of stable memory.
 /// Use `stable_grow` to request more stable memory if needed.
 pub fn stable_write(offset: u32, buf: &[u8]) {
-    unsafe {
-        super::ic0::stable_write(offset as i32, buf.as_ptr() as i32, buf.len() as i32);
-    }
+    CANISTER_STABLE_MEMORY.stable_write(offset, buf)
 }
 
 /// Similar to `stable_write` but with support for 64-bit addressed memory.
 pub fn stable64_write(offset: u64, buf: &[u8]) {
-    unsafe {
-        super::ic0::stable64_write(offset as i64, buf.as_ptr() as i64, buf.len() as i64);
-    }
+    CANISTER_STABLE_MEMORY.stable64_write(offset, buf)
 }
 
 /// Reads data from the stable memory location specified by an offset.
 pub fn stable_read(offset: u32, buf: &mut [u8]) {
-    unsafe {
-        super::ic0::stable_read(buf.as_ptr() as i32, offset as i32, buf.len() as i32);
-    }
+    CANISTER_STABLE_MEMORY.stable_read(offset, buf)
 }
 
 /// Similar to `stable_read` but with support for 64-bit addressed memory.
 pub fn stable64_read(offset: u64, buf: &mut [u8]) {
-    unsafe {
-        super::ic0::stable64_read(buf.as_ptr() as i64, offset as i64, buf.len() as i64);
-    }
+    CANISTER_STABLE_MEMORY.stable64_read(offset, buf)
 }
 
 /// Returns a copy of the stable memory.
@@ -105,32 +131,44 @@ pub fn stable_bytes() -> Vec<u8> {
 
 /// A writer to the stable memory.
 ///
+/// Warning: This will overwrite any existing data in stable memory as it writes, so ensure you set
+/// the `offset` value accordingly if you wish to preserve existing data.
+///
 /// Will attempt to grow the memory as it writes,
 /// and keep offsets and total capacity.
-pub struct StableWriter {
+pub struct StableWriter<M: StableMemory = CanisterStableMemory> {
     /// The offset of the next write.
     offset: usize,
 
     /// The capacity, in pages.
     capacity: u32,
+
+    /// The stable memory to write data to.
+    memory: M,
 }
 
 impl Default for StableWriter {
     fn default() -> Self {
-        let capacity = stable_size();
-
-        Self {
-            offset: 0,
-            capacity,
-        }
+        Self::with_memory(CanisterStableMemory::default(), 0)
     }
 }
 
-impl StableWriter {
+impl<M: StableMemory> StableWriter<M> {
+    /// Creates a new `StableWriter` which writes to the selected memory
+    pub fn with_memory(memory: M, offset: usize) -> Self {
+        let capacity = memory.stable_size();
+
+        Self {
+            offset,
+            capacity,
+            memory,
+        }
+    }
+
     /// Attempts to grow the memory by adding new pages.
-    pub fn grow(&mut self, added_pages: u32) -> Result<(), StableMemoryError> {
-        let old_page_count = stable_grow(added_pages)?;
-        self.capacity = old_page_count + added_pages;
+    pub fn grow(&mut self, new_pages: u32) -> Result<(), StableMemoryError> {
+        let old_page_count = self.memory.stable_grow(new_pages)?;
+        self.capacity = old_page_count + new_pages;
         Ok(())
     }
 
@@ -139,17 +177,23 @@ impl StableWriter {
     /// The only condition where this will
     /// error out is if it cannot grow the memory.
     pub fn write(&mut self, buf: &[u8]) -> Result<usize, StableMemoryError> {
-        if self.offset + buf.len() > ((self.capacity as usize) << 16) {
-            self.grow((buf.len() >> 16) as u32 + 1)?;
+        let required_capacity_bytes = self.offset + buf.len();
+        let required_capacity_pages = ((required_capacity_bytes + WASM_PAGE_SIZE_IN_BYTES - 1)
+            / WASM_PAGE_SIZE_IN_BYTES) as u32;
+        let current_pages = self.capacity;
+        let additional_pages_required = required_capacity_pages.saturating_sub(current_pages);
+
+        if additional_pages_required > 0 {
+            self.grow(additional_pages_required)?;
         }
 
-        stable_write(self.offset as u32, buf);
+        self.memory.stable_write(self.offset as u32, buf);
         self.offset += buf.len();
         Ok(buf.len())
     }
 }
 
-impl io::Write for StableWriter {
+impl<M: StableMemory> io::Write for StableWriter<M> {
     fn write(&mut self, buf: &[u8]) -> Result<usize, io::Error> {
         self.write(buf)
             .map_err(|e| io::Error::new(io::ErrorKind::OutOfMemory, e))
@@ -158,6 +202,44 @@ impl io::Write for StableWriter {
     fn flush(&mut self) -> Result<(), io::Error> {
         // Noop.
         Ok(())
+    }
+}
+
+/// A writer to the stable memory which first writes the bytes to an in memory buffer and flushes
+/// the buffer to stable memory each time it becomes full.
+///
+/// Warning: This will overwrite any existing data in stable memory as it writes, so ensure you set
+/// the `offset` value accordingly if you wish to preserve existing data.
+///
+/// Note: Each call to grow or write to stable memory is a relatively expensive operation, so pick a
+/// buffer size large enough to avoid excessive calls to stable memory.
+pub struct BufferedStableWriter<M: StableMemory = CanisterStableMemory> {
+    inner: io::BufWriter<StableWriter<M>>,
+}
+
+impl BufferedStableWriter {
+    /// Creates a new `BufferedStableWriter`
+    pub fn new(buffer_size: usize) -> BufferedStableWriter {
+        BufferedStableWriter::with_writer(buffer_size, StableWriter::default())
+    }
+}
+
+impl<M: StableMemory> BufferedStableWriter<M> {
+    /// Creates a new `BufferedStableWriter` which writes to the selected memory
+    pub fn with_writer(buffer_size: usize, writer: StableWriter<M>) -> BufferedStableWriter<M> {
+        BufferedStableWriter {
+            inner: io::BufWriter::with_capacity(buffer_size, writer),
+        }
+    }
+}
+
+impl<M: StableMemory> io::Write for BufferedStableWriter<M> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.inner.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.inner.flush()
     }
 }
 
---
+++ src/ic-cdk/src/api/stable/tests.rs
@@ -0,0 +1,154 @@
+use super::*;
+use std::rc::Rc;
+use std::sync::Mutex;
+
+#[derive(Default)]
+pub struct TestStableMemory {
+    memory: Rc<Mutex<Vec<u8>>>,
+}
+
+impl TestStableMemory {
+    pub fn new(memory: Rc<Mutex<Vec<u8>>>) -> TestStableMemory {
+        TestStableMemory { memory }
+    }
+}
+
+impl StableMemory for TestStableMemory {
+    fn stable_size(&self) -> u32 {
+        let bytes_len = self.memory.lock().unwrap().len();
+        let page_size = WASM_PAGE_SIZE_IN_BYTES as usize;
+        ((bytes_len + page_size - 1) / page_size) as u32
+    }
+
+    fn stable64_size(&self) -> u64 {
+        self.stable_size() as u64
+    }
+
+    fn stable_grow(&self, new_pages: u32) -> Result<u32, StableMemoryError> {
+        let new_bytes = new_pages as usize * WASM_PAGE_SIZE_IN_BYTES as usize;
+
+        let mut vec = self.memory.lock().unwrap();
+        let previous_len = vec.len();
+        let new_len = vec.len() + new_bytes;
+        vec.resize(new_len, 0);
+        Ok((previous_len / WASM_PAGE_SIZE_IN_BYTES as usize) as u32)
+    }
+
+    fn stable64_grow(&self, new_pages: u64) -> Result<u64, StableMemoryError> {
+        self.stable_grow(new_pages as u32).map(|len| len as u64)
+    }
+
+    fn stable_write(&self, offset: u32, buf: &[u8]) {
+        let offset = offset as usize;
+
+        let mut vec = self.memory.lock().unwrap();
+        if offset + buf.len() > vec.len() {
+            panic!("stable memory out of bounds");
+        }
+        vec[offset..(offset + buf.len())].clone_from_slice(buf);
+    }
+
+    fn stable64_write(&self, offset: u64, buf: &[u8]) {
+        self.stable_write(offset as u32, buf)
+    }
+
+    fn stable_read(&self, offset: u32, buf: &mut [u8]) {
+        let offset = offset as usize;
+
+        let vec = self.memory.lock().unwrap();
+        if offset + buf.len() < vec.len() {
+            panic!("stable memory out of bounds");
+        }
+        buf[..vec.len()].copy_from_slice(&vec[offset..]);
+    }
+
+    fn stable64_read(&self, offset: u64, buf: &mut [u8]) {
+        self.stable_read(offset as u32, buf)
+    }
+}
+
+mod stable_writer_tests {
+    use super::*;
+    use rstest::rstest;
+    use std::io::Write;
+
+    #[rstest]
+    #[case(None)]
+    #[case(Some(1))]
+    #[case(Some(10))]
+    #[case(Some(100))]
+    #[case(Some(1000))]
+    fn write_single_slice(#[case] buffer_size: Option<usize>) {
+        let memory = Rc::new(Mutex::new(Vec::new()));
+        let mut writer = build_writer(TestStableMemory::new(memory.clone()), buffer_size);
+
+        let bytes = vec![1; 100];
+
+        writer.write_all(&bytes).unwrap();
+        writer.flush().unwrap();
+
+        let result = &*memory.lock().unwrap();
+
+        assert_eq!(bytes, result[..bytes.len()]);
+    }
+
+    #[rstest]
+    #[case(None)]
+    #[case(Some(1))]
+    #[case(Some(10))]
+    #[case(Some(100))]
+    #[case(Some(1000))]
+    fn write_many_slices(#[case] buffer_size: Option<usize>) {
+        let memory = Rc::new(Mutex::new(Vec::new()));
+        let mut writer = build_writer(TestStableMemory::new(memory.clone()), buffer_size);
+
+        for i in 1..100 {
+            let bytes = vec![i as u8; i];
+            writer.write_all(&bytes).unwrap();
+        }
+        writer.flush().unwrap();
+
+        let result = &*memory.lock().unwrap();
+
+        let mut offset = 0;
+        for i in 1..100 {
+            let bytes = &result[offset..offset + i];
+            assert_eq!(bytes, vec![i as u8; i]);
+            offset += i;
+        }
+    }
+
+    #[rstest]
+    #[case(None)]
+    #[case(Some(1))]
+    #[case(Some(10))]
+    #[case(Some(100))]
+    #[case(Some(1000))]
+    fn ensure_only_requests_min_number_of_pages_required(#[case] buffer_size: Option<usize>) {
+        let memory = Rc::new(Mutex::new(Vec::new()));
+        let mut writer = build_writer(TestStableMemory::new(memory.clone()), buffer_size);
+
+        let mut total_bytes = 0;
+        for i in 1..10000 {
+            let bytes = vec![i as u8; i];
+            writer.write_all(&bytes).unwrap();
+            total_bytes += i;
+        }
+        writer.flush().unwrap();
+
+        let capacity_pages = TestStableMemory::new(memory).stable64_size();
+        let min_pages_required =
+            (total_bytes + WASM_PAGE_SIZE_IN_BYTES - 1) / WASM_PAGE_SIZE_IN_BYTES;
+
+        assert_eq!(capacity_pages, min_pages_required as u64);
+    }
+
+    fn build_writer(memory: TestStableMemory, buffer_size: Option<usize>) -> Box<dyn Write> {
+        let writer = StableWriter::with_memory(memory, 0);
+        if let Some(buffer_size) = buffer_size {
+            Box::new(BufferedStableWriter::with_writer(buffer_size, writer))
+        } else {
+            Box::new(writer)
+        }
+    }
+}
