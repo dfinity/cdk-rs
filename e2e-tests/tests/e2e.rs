04dd7ef62f7626e80088b5d304af283ed1b07270
Roman Kashitsyn <roman@dfinity.org>
2022-04-19 23:08:19 +0200

    update ic version and run e2e on macOS

--- e2e-tests/tests/e2e.rs
+++ e2e-tests/tests/e2e.rs
@@ -1,9 +1,71 @@
-use candid::{Decode, Encode};
+use candid::utils::{decode_args, encode_args, ArgumentDecoder, ArgumentEncoder};
 use ic_cdk_e2e_tests::cargo_build_canister;
-use ic_ic00_types::CanisterInstallMode;
-use ic_state_machine_tests::StateMachine;
+use ic_state_machine_tests::{CanisterId, StateMachine, UserError, WasmResult};
 use serde_bytes::ByteBuf;
 
+#[derive(Debug)]
+enum CallError {
+    Reject(String),
+    UserError(UserError),
+}
+
+/// A helper function that we use to implement both [`call_candid`] and
+/// [`query_candid`].
+fn with_candid<Input, Output>(
+    input: Input,
+    f: impl FnOnce(Vec<u8>) -> Result<WasmResult, UserError>,
+) -> Result<Output, CallError>
+where
+    Input: ArgumentEncoder,
+    Output: for<'a> ArgumentDecoder<'a>,
+{
+    let in_bytes = encode_args(input).expect("failed to encode args");
+    match f(in_bytes) {
+        Ok(WasmResult::Reply(out_bytes)) => Ok(decode_args(&out_bytes).unwrap_or_else(|e| {
+            panic!(
+                "Failed to decode bytes {:?} as candid type: {}",
+                std::any::type_name::<Output>(),
+                e
+            )
+        })),
+        Ok(WasmResult::Reject(message)) => Err(CallError::Reject(message)),
+        Err(user_error) => Err(CallError::UserError(user_error)),
+    }
+}
+
+/// Call a canister candid method.
+fn call_candid<Input, Output>(
+    env: &StateMachine,
+    canister_id: CanisterId,
+    method: &str,
+    input: Input,
+) -> Result<Output, CallError>
+where
+    Input: ArgumentEncoder,
+    Output: for<'a> ArgumentDecoder<'a>,
+{
+    with_candid(input, |bytes| {
+        env.execute_ingress(canister_id, method, bytes)
+    })
+}
+
+/// Query a canister candid method.
+fn query_candid<Input, Output>(
+    env: &StateMachine,
+    canister_id: CanisterId,
+    method: &str,
+    input: Input,
+) -> Result<Output, CallError>
+where
+    Input: ArgumentEncoder,
+    Output: for<'a> ArgumentDecoder<'a>,
+{
+    with_candid(input, |bytes| env.query(canister_id, method, bytes))
+}
+
+/// Checks that a canister that uses [`ic_cdk::storage::stable_store`]
+/// and [`ic_cdk::storage::stable_restore`] functions can keep its data
+/// across upgrades.
 #[test]
 fn test_storage_roundtrip() {
     let env = StateMachine::new();
@@ -12,24 +74,13 @@ fn test_storage_roundtrip() {
         .install_canister(kv_store_wasm.clone(), vec![], None)
         .unwrap();
 
-    env.execute_ingress(canister_id, "insert", Encode!(&"candid", &b"did").unwrap())
+    let () = call_candid(&env, canister_id, "insert", (&"candid", &b"did"))
         .expect("failed to insert 'candid'");
 
-    env.install_wasm_in_mode(
-        canister_id,
-        CanisterInstallMode::Upgrade,
-        kv_store_wasm,
-        vec![],
-    )
-    .expect("failed to upgrade the simple-kv-store canister");
+    env.upgrade_canister(canister_id, kv_store_wasm, vec![])
+        .expect("failed to upgrade the simple-kv-store canister");
 
-    let result = Decode!(
-        &env.query(canister_id, "lookup", Encode!(&"candid").unwrap(),)
-            .unwrap()
-            .bytes(),
-        Option<ByteBuf>
-    )
-    .unwrap();
-
+    let (result,): (Option<ByteBuf>,) =
+        query_candid(&env, canister_id, "lookup", (&"candid",)).expect("failed to lookup 'candid'");
     assert_eq!(result, Some(ByteBuf::from(b"did".to_vec())));
 }
