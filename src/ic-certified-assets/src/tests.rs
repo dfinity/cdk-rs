4db32a2635fd64b5ec7bc5fcb95253636146474c
Roman Kashitsyn <roman@dfinity.org>
2022-05-06 23:17:42 +0200

    refactor: move pure assets certification code into a module
    This change is a large refactoring of the ic-certified-assets package.
    The motivation for this change is to make the code more testable and
    organized before adding caching logic.
    
    The general idea behind the refactoring it to split out the "state
    machine" logic into pure code that does not depend on Rust CDK and
    accepts all the environment (time, certificates, etc) as function
    parameters. This logic moved to `state_machine.rs`.
    
    `lib.rs` now contains the minimal glue to connect the state machine to
    the Rust CDK interface. Eventually this part should move back to
    https://github.com/dfinity/certified-assets, making
    `ic-certified-assets` package independent from Rust CDK.
    
    The refactoring does not affect the certification logic, all we do is
    moving the code around, improving memory-correctness guarantees (no more
    RefCells in the state), and adding tests.

--- src/ic-certified-assets/src/tests.rs
+++ src/ic-certified-assets/src/tests.rs
@@ -1,24 +1,83 @@
-use crate::*;
-
-use std::panic::catch_unwind;
+use crate::state_machine::State;
+use crate::types::{
+    BatchOperation, CommitBatchArguments, CreateAssetArguments, CreateChunkArg, HttpRequest,
+    HttpResponse, SetAssetContentArguments,
+};
+use serde_bytes::ByteBuf;
 
 #[test]
-fn check_url_decode() {
-    assert_eq!(
-        url_decode("/%"),
-        Err(UrlDecodeError::InvalidPercentEncoding)
+fn can_create_assets_using_batch_api() {
+    let mut state = State::default();
+    let time_now = 100_000_000_000;
+
+    let batch_id = state.create_batch(time_now);
+
+    const BODY: &[u8] = b"<!DOCTYPE html><html></html>";
+
+    let chunk_id = state
+        .create_chunk(
+            CreateChunkArg {
+                batch_id: batch_id.clone(),
+                content: ByteBuf::from(BODY.to_vec()),
+            },
+            time_now,
+        )
+        .unwrap();
+
+    state
+        .commit_batch(
+            CommitBatchArguments {
+                batch_id: batch_id.clone(),
+                operations: vec![
+                    BatchOperation::CreateAsset(CreateAssetArguments {
+                        key: "/contents.html".to_string(),
+                        content_type: "text/html".to_string(),
+                    }),
+                    BatchOperation::SetAssetContent({
+                        SetAssetContentArguments {
+                            key: "/contents.html".to_string(),
+                            content_encoding: "identity".to_string(),
+                            chunk_ids: vec![chunk_id.clone()],
+                            sha256: None,
+                        }
+                    }),
+                ],
+            },
+            time_now,
+        )
+        .unwrap();
+
+    let response = state.http_request(
+        HttpRequest {
+            body: ByteBuf::new(),
+            headers: vec![("Accept-Encoding".to_string(), "gzip,identity".to_string())],
+            method: "GET".to_string(),
+            url: "/contents.html".to_string(),
+        },
+        &[],
     );
-    assert_eq!(url_decode("/%%"), Ok("/%".to_string()));
-    assert_eq!(url_decode("/%20a"), Ok("/ a".to_string()));
-    assert_eq!(
-        url_decode("/%%+a%20+%@"),
-        Err(UrlDecodeError::InvalidPercentEncoding)
+
+    assert_eq!(response.status_code, 200);
+    assert_eq!(response.body.as_ref(), BODY);
+
+    // Try to update a completed batch.
+    let error_msg = state
+        .create_chunk(
+            CreateChunkArg {
+                batch_id,
+                content: ByteBuf::new(),
+            },
+            time_now,
+        )
+        .unwrap_err();
+
+    let expected = "batch not found";
+    assert!(
+        error_msg.contains(expected),
+        "expected '{}' error, got: {}",
+        expected,
+        error_msg
     );
-    assert_eq!(
-        url_decode("/has%percent.txt"),
-        Err(UrlDecodeError::InvalidPercentEncoding)
-    );
-    assert_eq!(url_decode("/%e6"), Ok("/æ".to_string()));
 }
 
 #[test]
@@ -38,24 +97,33 @@ fn redirects_cleanly() {
             .iter()
             .any(|(key, value)| key == "Location" && value == expected));
     }
+
+    let state = State::default();
+    let fake_cert = [0xca, 0xfe];
+
     assert_308(
-        &http_request(fake("aaaaa-aa.raw.ic0.app")),
+        &state.http_request(fake("aaaaa-aa.raw.ic0.app"), &fake_cert),
         "https://aaaaa-aa.ic0.app/asset.blob",
     );
     assert_308(
-        &http_request(fake("my.http.files.raw.ic0.app")),
+        &state.http_request(fake("my.http.files.raw.ic0.app"), &fake_cert),
         "https://my.http.files.ic0.app/asset.blob",
     );
     assert_308(
-        &http_request(fake("raw.ic0.app.raw.ic0.app")),
+        &state.http_request(fake("raw.ic0.app.raw.ic0.app"), &fake_cert),
         "https://raw.ic0.app.ic0.app/asset.blob",
     );
     assert_308(
-        &http_request(fake("raw.ic0.app")), // for ?canisterId=
+        &state.http_request(fake("raw.ic0.app"), &fake_cert), // for ?canisterId=
         "https://ic0.app/asset.blob",
     );
-    let no_redirect = catch_unwind(|| http_request(fake("raw.ic0.app.ic0.app")).status_code);
-    assert!(!matches!(no_redirect, Ok(308)));
-    let no_redirect2 = catch_unwind(|| http_request(fake("straw.ic0.app")).status_code);
-    assert!(!matches!(no_redirect2, Ok(308)));
+    let no_redirect = state
+        .http_request(fake("raw.ic0.app.ic0.app"), &fake_cert)
+        .status_code;
+    assert!(!matches!(no_redirect, 308));
+
+    let no_redirect2 = state
+        .http_request(fake("straw.ic0.app"), &fake_cert)
+        .status_code;
+    assert!(!matches!(no_redirect2, 308));
 }
