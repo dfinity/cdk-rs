df71cf27e2f77feadcaddf5675f20fd745f41ce5
Linwei Shang <linwei.shang@dfinity.org>
2022-05-18 16:57:51 -0400

    fmt

--- e2e-tests/canisters/reverse.rs
+++ e2e-tests/canisters/reverse.rs
@@ -4,13 +4,7 @@ use ic_cdk::api::call::{arg_data_raw, arg_data_raw_size, reply_raw};
 fn reverse() {
     let arg_bytes: Vec<u8> = arg_data_raw();
     assert_eq!(arg_bytes.len(), arg_data_raw_size());
-    reply_raw(
-        arg_bytes
-            .into_iter()
-            .rev()
-            .collect::<Vec<_>>()
-            .as_ref(),
-    );
+    reply_raw(arg_bytes.into_iter().rev().collect::<Vec<_>>().as_ref());
 }
 
 fn main() {}
