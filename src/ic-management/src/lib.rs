4e2714fd59571a5529da5c861b75f1da40567c91
Linwei Shang <linwei.shang@dfinity.org>
2022-07-18 10:50:14 -0700

    tuple

--- src/ic-management/src/lib.rs
+++ src/ic-management/src/lib.rs
@@ -1,8 +1,7 @@
+use ic_cdk::api::call::{call, CallResult};
 use ic_cdk::export::Principal;
-use ::ic_cdk::api::call::{call, CallResult};
 
 // raw_rand : () -> (blob);
-pub async fn raw_rand() -> CallResult<Vec<u8>> {
-    call(Principal::management_canister(), "start_canister", ())
-        .await
+pub async fn raw_rand() -> CallResult<(Vec<u8>,)> {
+    call(Principal::management_canister(), "start_canister", ()).await
 }
