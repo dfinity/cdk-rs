b1408f81cb28b7b47009ba60e005ee10a39c7476
tim-dfn <97036153+tim-dfn@users.noreply.github.com>
2022-07-26 18:03:21 +0200

    feat: Implement common traits for RejectionCode (#294)
    * implement common types
    
    * remove serialize
    
    * changelog
    
    Co-authored-by: Linwei Shang <linwei.shang@dfinity.org>

--- src/ic-cdk/src/api/call.rs
+++ src/ic-cdk/src/api/call.rs
@@ -130,7 +130,7 @@ use rc::{InnerCell, WasmCell};
 /// These can be obtained either using `reject_code()` or `reject_result()`.
 #[allow(missing_docs)]
 #[repr(i32)]
-#[derive(Debug, CandidType, Deserialize, Clone, Copy)]
+#[derive(CandidType, Deserialize, Clone, Copy, Hash, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub enum RejectionCode {
     NoError = 0,
 
