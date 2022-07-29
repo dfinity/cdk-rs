ae73b5817b12477f147a33b69dabf066e5d80964
Linwei Shang <linwei.shang@dfinity.org>
2022-07-29 12:14:29 -0400

    mv

---
+++ src/ic-cdk/src/api/management_canister/mod.rs
@@ -0,0 +1,9 @@
+pub mod bitcoin;
+pub mod http_request;
+pub mod main;
+pub mod provisional;
+pub mod threshold_ecdsa;
+
+pub use main::*;
+
+// TODO: call or call_with_payment?
--- src/ic-management/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod bitcoin;
-pub mod http_request;
-pub mod main;
-pub mod provisional;
-pub mod threshold_ecdsa;
-
-pub use main::*;
-
-// TODO: call or call_with_payment?
