8f16606da014bc253e3d9742939ad577481dfd8c
Linwei Shang <linwei.shang@dfinity.org>
2022-08-03 17:00:48 -0400

    Revert change of e2e

--- e2e-tests/tests/e2e.rs
+++ e2e-tests/tests/e2e.rs
@@ -1,8 +1,70 @@
+pub use candid::utils::{decode_args, encode_args, ArgumentDecoder, ArgumentEncoder};
 use ic_cdk_e2e_tests::cargo_build_canister;
+pub use ic_state_machine_tests::{CanisterId, ErrorCode, StateMachine, UserError, WasmResult};
 use serde_bytes::ByteBuf;
 
-mod common;
-use common::*;
+#[derive(Debug)]
+pub enum CallError {
+    Reject(String),
+    UserError(UserError),
+}
+
+/// A helper function that we use to implement both [`call_candid`] and
+/// [`query_candid`].
+fn with_candid<Input, Output>(
+    input: Input,
+    f: impl FnOnce(Vec<u8>) -> Result<WasmResult, UserError>,
+) -> Result<Output, CallError>
+where
+    Input: ArgumentEncoder,
+    Output: for<'a> ArgumentDecoder<'a>,
+{
+    let in_bytes = encode_args(input).expect("failed to encode args");
+    match f(in_bytes) {
+        Ok(WasmResult::Reply(out_bytes)) => Ok(decode_args(&out_bytes).unwrap_or_else(|e| {
+            panic!(
+                "Failed to decode bytes {:?} as candid type: {}",
+                std::any::type_name::<Output>(),
+                e
+            )
+        })),
+        Ok(WasmResult::Reject(message)) => Err(CallError::Reject(message)),
+        Err(user_error) => Err(CallError::UserError(user_error)),
+    }
+}
+
+/// Call a canister candid method.
+fn call_candid<Input, Output>(
+    env: &StateMachine,
+    canister_id: CanisterId,
+    method: &str,
+    input: Input,
+) -> Result<Output, CallError>
+where
+    Input: ArgumentEncoder,
+    Output: for<'a> ArgumentDecoder<'a>,
+{
+    with_candid(input, |bytes| {
+        env.execute_ingress(canister_id, method, bytes)
+    })
+}
+
+/// Query a canister candid method.
+// This function is indeed used by tests.
+// Might be a bug with cargo.
+#[allow(dead_code)]
+fn query_candid<Input, Output>(
+    env: &StateMachine,
+    canister_id: CanisterId,
+    method: &str,
+    input: Input,
+) -> Result<Output, CallError>
+where
+    Input: ArgumentEncoder,
+    Output: for<'a> ArgumentDecoder<'a>,
+{
+    with_candid(input, |bytes| env.query(canister_id, method, bytes))
+}
 
 /// Checks that a canister that uses [`ic_cdk::storage::stable_store`]
 /// and [`ic_cdk::storage::stable_restore`] functions can keep its data
