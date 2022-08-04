1c66022521ba88820cfed9ca00f76a3ea62828c5
Linwei Shang <linwei.shang@dfinity.org>
2022-08-04 17:04:37 -0400

    doc provisional

--- src/ic-cdk/src/api/management_canister/provisional.rs
+++ src/ic-cdk/src/api/management_canister/provisional.rs
@@ -1,27 +1,34 @@
+//! Provisional functions only available in local development instances.
+
 use crate::api::call::{call, CallResult};
 use candid::{CandidType, Nat, Principal};
 use serde::Deserialize;
 
 pub use super::main::{CanisterId, CanisterIdRecord, CanisterSettings};
 
+/// See [`provisional_create_canister_with_cycles`].
 #[derive(Default, Clone, CandidType, Deserialize, Debug)]
 pub struct ProvisionalCreateCanisterWithCyclesArgument {
+    /// The created canister will have this amount of cycles.
     pub amount: Option<Nat>,
+    /// See [`CanisterSettings`].
     pub settings: Option<CanisterSettings>,
 }
 
+/// See [`provisional_top_up_canister`]
 #[derive(Clone, CandidType, Deserialize, Debug)]
 pub struct ProvisionalTopUpCanisterArgument {
+    /// Canister ID.
     pub canister_id: CanisterId,
+    /// Amount of cycles to be added.
     pub amount: Nat,
 }
 
-/**
-provisional_create_canister_with_cycles : (record {
-  amount: opt nat;
-  settings : opt canister_settings
-}) -> (record {canister_id : canister_id});
-*/
+/// Create a new canister with specified amout of cycles balance.
+///
+/// This method is only available in local development instances.
+///
+/// See [IC method `provisional_create_canister_with_cycles`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-provisional_create_canister_with_cycles).
 pub async fn provisional_create_canister_with_cycles(
     arg: ProvisionalCreateCanisterWithCyclesArgument,
 ) -> CallResult<(CanisterIdRecord,)> {
@@ -33,10 +40,11 @@ pub async fn provisional_create_canister_with_cycles(
     .await
 }
 
-/**
-provisional_top_up_canister :
-  (record { canister_id: canister_id; amount: nat }) -> ();
-*/
+/// Add cycles to a canister.
+///
+/// This method is only available in local development instances.
+///
+/// See [IC method `provisional_top_up_canister`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-provisional_top_up_canister).
 pub async fn provisional_top_up_canister(arg: ProvisionalTopUpCanisterArgument) -> CallResult<()> {
     call(
         Principal::management_canister(),
