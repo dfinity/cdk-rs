1f69e3d0aa99fc9563ead94469859ecde1fd733f
Linwei Shang <linwei.shang@dfinity.org>
2022-08-04 17:48:31 -0400

    doc http_request

--- src/ic-cdk/src/api/management_canister/http_request.rs
+++ src/ic-cdk/src/api/management_canister/http_request.rs
@@ -1,52 +1,68 @@
+//! Canister HTTP request.
+
 use crate::api::call::{call_with_payment128, CallResult};
 use candid::{CandidType, Principal};
 use serde::{Deserialize, Serialize};
 
+/// HTTP header.
 #[derive(CandidType, Clone, Deserialize, Debug, Eq, Hash, PartialEq, Serialize)]
 pub struct HttpHeader {
+    /// Name
     pub name: String,
+    /// Value
     pub value: String,
 }
 
+/// HTTP method.
+///
+/// Currently support following methods.
 #[derive(Clone, Debug, PartialEq, CandidType, Eq, Hash, Serialize, Deserialize)]
 pub enum HttpMethod {
+    /// GET
     GET,
+    /// POST
     POST,
+    /// HEAD
     HEAD,
 }
 
-/*
-record {
-  url : text;
-  max_response_bytes: opt nat64;
-  method : variant { get; head; post };
-  headers: vec http_header;
-  body : opt blob;
-  transform : opt variant {
-    function: func (http_response) -> (http_response) query
-  };
-}
-*/
+/// See [`http_request`].
 #[derive(CandidType, Deserialize, Debug)]
 pub struct CanisterHttpRequestArgument {
+    /// The requested URL.
     pub url: String,
+    /// The maximal size of the response in bytes. If None, 2MiB will be the limit.
     pub max_response_bytes: Option<u64>,
     // TODO: Different name in the Spec.
+    /// The method of HTTP request.
     pub http_method: HttpMethod,
+    /// List of HTTP request headers and their corresponding values.
     pub headers: Vec<HttpHeader>,
+    /// Optionally provide request body.
     pub body: Option<Vec<u8>>,
     // TODO: Here is a discrepancy between System API and the implementation.
+    /// Name of the transform function which is `func (http_response) -> (http_response) query`.
     pub transform_method_name: Option<String>,
 }
 
+/// The returned HTTP response.
 #[derive(CandidType, Clone, Debug, PartialEq, Eq, Hash, Deserialize)]
 pub struct HttpResponse {
     // TODO: Different type in the Spec.
+    /// The response status (e.g., 200, 404).
     pub status: u64,
+    /// List of HTTP response headers and their corresponding values.
     pub headers: Vec<HttpHeader>,
+    /// The response’s body.
     pub body: Vec<u8>,
 }
 
+/// Make an HTTP request to a given URL and return the HTTP response, possibly after a transformation.
+///
+/// This call requires cycles payment. The required cycles is a function of the request size and max_response_bytes.
+/// See source code for the exact function.
+///
+/// See [IC method `http_request`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-http_request).
 pub async fn http_request(arg: CanisterHttpRequestArgument) -> CallResult<(HttpResponse,)> {
     let cycles = http_request_required_cycles(&arg);
     call_with_payment128(
@@ -65,7 +81,8 @@ fn http_request_required_cycles(arg: &CanisterHttpRequestArgument) -> u128 {
     };
     let arg_raw = candid::utils::encode_args((arg,)).expect("Failed to encode arguments.");
     // TODO: this formula should be documented somewhere
-    400_000_000u128 + 100_000u128 * (arg_raw.len() as u128 + 12u128 + max_response_bytes)
+    // 12 is "http_request".len().
+    400_000_000u128 + 100_000u128 * (arg_raw.len() as u128 + 12 + max_response_bytes)
 }
 
 #[cfg(test)]
