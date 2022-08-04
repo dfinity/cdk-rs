3141bd86b96652e546ae008838d3d1185eed66dc
Linwei Shang <linwei.shang@dfinity.org>
2022-08-04 16:19:47 -0400

    doc main

--- src/ic-cdk/src/api/management_canister/main/mod.rs
+++ src/ic-cdk/src/api/management_canister/main/mod.rs
@@ -1,3 +1,10 @@
+//! The main functionalities in [the IC management canister][1].
+//!
+//! Most of the functions are for managing canister lifecycle.
+//! [`raw_rand`] is also included in this module.
+//!
+//! [1]: https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-management-canister
+
 use crate::api::call::{call, call_with_payment128, CallResult};
 use candid::Principal;
 
@@ -7,9 +14,9 @@ pub use types::*;
 // https://internetcomputer.org/docs/current/developer-docs/deploy/computation-and-storage-costs
 const CREATE_CANISTER_CYCLES: u128 = 1_000_000_000_000u128;
 
-/// create_canister : (record {
-///   settings : opt canister_settings
-/// }) -> (record {canister_id : canister_id});
+/// Register a new canister and get its canister id.
+///
+/// See [IC method `create_canister`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-create_canister).
 pub async fn create_canister(arg: CreateCanisterArgument) -> CallResult<(CanisterIdRecord,)> {
     call_with_payment128(
         Principal::management_canister(),
@@ -20,57 +27,61 @@ pub async fn create_canister(arg: CreateCanisterArgument) -> CallResult<(Caniste
     .await
 }
 
-/// update_settings : (record {
-///   canister_id : principal;
-///   settings : canister_settings
-/// }) -> ();
+/// Update the settings of a canister.
+///
+/// See [IC method `update_settings`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-update_settings).
 pub async fn update_settings(arg: UpdateSettingsArgument) -> CallResult<()> {
     call(Principal::management_canister(), "update_settings", (arg,)).await
 }
 
-/// install_code : (record {
-///   mode : variant {install; reinstall; upgrade};
-///   canister_id : canister_id;
-///   wasm_module : wasm_module;
-///   arg : blob;
-/// }) -> ();
+/// Install code into a canister.
+///
+/// See [IC method `install_code`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-install_code).
 pub async fn install_code(arg: InstallCodeArgument) -> CallResult<()> {
     call(Principal::management_canister(), "install_code", (arg,)).await
 }
 
-/// uninstall_code : (record {canister_id : canister_id}) -> ();
+/// Remove a canister's code and state, making the canister empty again.
+///
+/// See [IC method `uninstall_code`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-uninstall_code)
 pub async fn uninstall_code(arg: CanisterIdRecord) -> CallResult<()> {
     call(Principal::management_canister(), "uninstall_code", (arg,)).await
 }
 
-/// start_canister : (record {canister_id : canister_id}) -> ();
+/// Start a canister if the canister status was `stopped` or `stopping`.
+///
+/// See [IC method `start_canister`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-start_canister)
 pub async fn start_canister(arg: CanisterIdRecord) -> CallResult<()> {
     call(Principal::management_canister(), "start_canister", (arg,)).await
 }
 
-/// stop_canister : (record {canister_id : canister_id}) -> ();
+/// Stop a canister.
+///
+/// See [IC method `stop_canister`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-stop_canister)
 pub async fn stop_canister(arg: CanisterIdRecord) -> CallResult<()> {
     call(Principal::management_canister(), "stop_canister", (arg,)).await
 }
 
-/// canister_status : (record {canister_id : canister_id}) -> (record {
-///   status : variant { running; stopping; stopped };
-///   settings: definite_canister_settings;
-///   module_hash: opt blob;
-///   memory_size: nat;
-///   cycles: nat;
-///   idle_cycles_burned_per_day: nat;
-/// });
+/// Get status information about the canister.
+///
+/// See [IC method `canister_status`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-canister_status)
 pub async fn canister_status(arg: CanisterIdRecord) -> CallResult<(CanisterStatusReply,)> {
     call(Principal::management_canister(), "canister_status", (arg,)).await
 }
 
-/// delete_canister : (record {canister_id : canister_id}) -> ();
+/// Delete a canister from the IC.
+///
+/// See [IC method `delete_canister`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-delete_canister)
 pub async fn delete_canister(arg: CanisterIdRecord) -> CallResult<()> {
     call(Principal::management_canister(), "delete_canister", (arg,)).await
 }
 
-/// deposit_cycles : (record {canister_id : canister_id}) -> ();
+/// Deposit cycles into the specified canister.
+///
+/// Note that, beyond the argument as specified in the interface description,
+/// there is a second parameter `cycles` which is the amount of cycles to be deposited.
+///
+/// See [IC method `deposit_cycles`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-deposit_cycles)
 pub async fn deposit_cycles(arg: CanisterIdRecord, cycles: u128) -> CallResult<()> {
     call_with_payment128(
         Principal::management_canister(),
@@ -81,7 +92,9 @@ pub async fn deposit_cycles(arg: CanisterIdRecord, cycles: u128) -> CallResult<(
     .await
 }
 
-/// raw_rand : () -> (blob);
+/// Get 32 pseudo-random bytes.
+///
+/// See [IC method `raw_rand`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-raw_rand)
 pub async fn raw_rand() -> CallResult<(Vec<u8>,)> {
     call(Principal::management_canister(), "raw_rand", ()).await
 }
