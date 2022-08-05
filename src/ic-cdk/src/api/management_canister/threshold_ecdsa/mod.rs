47e46aae6e8372f790144d08f41a6fbbd2efb9fa
Linwei Shang <linwei.shang@dfinity.org>
2022-08-05 09:20:10 -0400

    doc ecdsa

--- src/ic-cdk/src/api/management_canister/threshold_ecdsa/mod.rs
+++ src/ic-cdk/src/api/management_canister/threshold_ecdsa/mod.rs
@@ -1,27 +1,23 @@
+//! The ECDSA API.
+
 use crate::api::call::{call, CallResult};
 use candid::Principal;
 
 mod types;
 pub use types::*;
 
-/**
-  ecdsa_public_key : (record {
-    canister_id : opt canister_id;
-    derivation_path : vec blob;
-    key_id : record { curve: ecdsa_curve; name: text };
-  }) -> (record { public_key : blob; chain_code : blob; });
-*/
-pub async fn ecdsa_public_key(arg: EcdsaPublicKeyArgument) -> CallResult<(EcdsaPublicKeyReply,)> {
+/// Return a SEC1 encoded ECDSA public key for the given canister using the given derivation path.
+///
+/// See [IC method `ecdsa_public_key`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-ecdsa_public_key).
+pub async fn ecdsa_public_key(
+    arg: EcdsaPublicKeyArgument,
+) -> CallResult<(EcdsaPublicKeyResponse,)> {
     call(Principal::management_canister(), "ecdsa_public_key", (arg,)).await
 }
 
-/**
-  sign_with_ecdsa : (record {
-    message_hash : blob;
-    derivation_path : vec blob;
-    key_id : record { curve: ecdsa_curve; name: text };
-  }) -> (record { signature : blob });
-*/
-pub async fn sign_with_ecdsa(arg: SignWithEcdsaArgument) -> CallResult<(SignWithEcdsaReply,)> {
+/// Return a new ECDSA signature of the given message_hash that can be separately verified against a derived ECDSA public key.
+///
+/// See [IC method `sign_with_ecdsa`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-sign_with_ecdsa).
+pub async fn sign_with_ecdsa(arg: SignWithEcdsaArgument) -> CallResult<(SignWithEcdsaResponse,)> {
     call(Principal::management_canister(), "sign_with_ecdsa", (arg,)).await
 }
--- src/ic-cdk/src/api/management_canister/threshold_ecdsa/types.rs
+++ src/ic-cdk/src/api/management_canister/threshold_ecdsa/types.rs
@@ -3,39 +3,57 @@ use serde::{Deserialize, Serialize};
 
 use super::super::main::CanisterId;
 
+/// Argument type of [ecdsa_public_key](super::ecdsa_public_key).
 #[derive(CandidType, Serialize, Debug)]
 pub struct EcdsaPublicKeyArgument {
+    /// Canister id, default to the canister id of the caller if None.
     pub canister_id: Option<CanisterId>,
+    /// A vector of variable length byte strings.
     pub derivation_path: Vec<Vec<u8>>,
+    /// See [`EcdsaKeyId`].
     pub key_id: EcdsaKeyId,
 }
 
+/// Response Type of [ecdsa_public_key](super::ecdsa_public_key).
 #[derive(CandidType, Deserialize, Debug)]
-pub struct EcdsaPublicKeyReply {
+pub struct EcdsaPublicKeyResponse {
+    /// An ECDSA public key encoded in SEC1 compressed form.
     pub public_key: Vec<u8>,
+    /// Can be used to deterministically derive child keys of the public_key.
     pub chain_code: Vec<u8>,
 }
 
+/// Argument type of [sign_with_ecdsa](super::sign_with_ecdsa).
 #[derive(CandidType, Serialize, Debug)]
 pub struct SignWithEcdsaArgument {
+    /// Hash of the message with length of 32 bytes.
     pub message_hash: Vec<u8>,
+    /// A vector of variable length byte strings.
     pub derivation_path: Vec<Vec<u8>>,
+    /// See [`EcdsaKeyId`].
     pub key_id: EcdsaKeyId,
 }
 
+/// Response type of [sign_with_ecdsa](super::sign_with_ecdsa).
 #[derive(CandidType, Deserialize, Debug)]
-pub struct SignWithEcdsaReply {
+pub struct SignWithEcdsaResponse {
+    /// Encoded as the concatenation of the SEC1 encodings of the two values r and s.
     pub signature: Vec<u8>,
 }
 
+/// ECDSA KeyId.
 #[derive(CandidType, Serialize, Debug, Clone)]
 pub struct EcdsaKeyId {
+    /// See [`EcdsaCurve`].
     pub curve: EcdsaCurve,
+    /// Name.
     pub name: String,
 }
 
+/// ECDSA Curve.
 #[derive(CandidType, Serialize, Debug, Clone)]
 pub enum EcdsaCurve {
+    /// secp256k1
     #[serde(rename = "secp256k1")]
     Secp256k1,
 }
