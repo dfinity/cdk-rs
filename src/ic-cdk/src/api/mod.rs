ad636d370dc97c4237f1fc3a809ec698f5ddfa8c
Severin Siffert <severin.siffert@dfinity.org>
2023-03-31 17:54:19 +0200

    doesnt work

--- src/ic-cdk/src/api/mod.rs
+++ src/ic-cdk/src/api/mod.rs
@@ -138,5 +138,6 @@ pub fn canister_version() -> u64 {
 pub fn is_controller(principal: &Principal) -> bool {
     let slice = principal.as_slice();
     // SAFETY: ic0.is_controller is always safe to call.
-    unsafe { ic0::is_controller(slice.as_ptr() as i32, slice.len() as i32) } != 0
+    let is_controller = unsafe { ic0::is_controller(slice.as_ptr() as i32, slice.len() as i32) };
+    c != 0
 }
