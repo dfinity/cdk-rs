74c9d0a4fb3dbc477b624a6e0c0b432e702b8229
Severin Siffert <severin.siffert@dfinity.org>
2023-03-31 17:04:14 +0200

    next try

--- src/ic-cdk/src/api/mod.rs
+++ src/ic-cdk/src/api/mod.rs
@@ -138,6 +138,5 @@ pub fn canister_version() -> u64 {
 pub fn is_controller(principal: &Principal) -> bool {
     let slice = principal.as_slice();
     // SAFETY: ic0.is_controller is always safe to call.
-    let is_controller = unsafe { ic0::is_controller(slice.as_ptr() as i32, slice.len() as i32) };
-    is_controller != 0
+    unsafe { ic0::is_controller(slice.as_ptr() as i32, slice.len() as i32) }
 }
