a582289a7f07b9293d1785dcf5fe3c1c32cb3a03
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2023-05-10 11:11:01 +0200

    switch to RefCell

--- src/ic-cdk-http-kit/src/storage.rs
+++ src/ic-cdk-http-kit/src/storage.rs
@@ -2,25 +2,25 @@ use crate::mock::{hash, Mock};
 use ic_cdk::api::management_canister::http_request::{
     CanisterHttpRequestArgument, HttpResponse, TransformArgs,
 };
+use std::cell::RefCell;
 use std::collections::HashMap;
-use std::sync::RwLock;
 
 thread_local! {
-    static MOCKS: RwLock<HashMap<String, Mock>> = RwLock::new(HashMap::new());
+    static MOCKS: RefCell<HashMap<String, Mock>> = RefCell::new(HashMap::new());
 
-    static TRANSFORM_FUNCTIONS: RwLock<HashMap<String, Box<TransformFn>>> = RwLock::new(HashMap::new());
+    static TRANSFORM_FUNCTIONS: RefCell<HashMap<String, Box<TransformFn>>> = RefCell::new(HashMap::new());
 }
 
 /// Inserts the provided mock into a thread-local hashmap.
 pub(crate) fn mock_insert(mock: Mock) {
     MOCKS.with(|cell| {
-        cell.write().unwrap().insert(hash(&mock.request), mock);
+        cell.borrow_mut().insert(hash(&mock.request), mock);
     });
 }
 
 /// Returns a cloned mock from the thread-local hashmap that corresponds to the provided request.
 pub(crate) fn mock_get(request: &CanisterHttpRequestArgument) -> Option<Mock> {
-    MOCKS.with(|cell| cell.read().unwrap().get(&hash(request)).cloned())
+    MOCKS.with(|cell| cell.borrow().get(&hash(request)).cloned())
 }
 
 type TransformFn = dyn Fn(TransformArgs) -> HttpResponse + 'static;
@@ -31,21 +31,21 @@ pub(crate) fn transform_function_insert(name: String, func: Box<TransformFn>) {
     TRANSFORM_FUNCTIONS.with(|cell| {
         // This is a workaround to prevent the transform function from being
         // overridden while it is being executed.
-        if cell.read().unwrap().get(&name).is_none() {
-            cell.write().unwrap().insert(name, func);
+        if cell.borrow().get(&name).is_none() {
+            cell.borrow_mut().insert(name, func);
         }
     });
 }
 
 /// Executes the transform function that corresponds to the provided name.
 pub(crate) fn transform_function_call(name: String, arg: TransformArgs) -> Option<HttpResponse> {
-    TRANSFORM_FUNCTIONS.with(|cell| cell.read().unwrap().get(&name).map(|f| f(arg)))
+    TRANSFORM_FUNCTIONS.with(|cell| cell.borrow().get(&name).map(|f| f(arg)))
 }
 
 /// Returns a sorted list of transform function names.
 pub(crate) fn transform_function_names() -> Vec<String> {
     TRANSFORM_FUNCTIONS.with(|cell| {
-        let mut names: Vec<String> = cell.read().unwrap().keys().cloned().collect();
+        let mut names: Vec<String> = cell.borrow().keys().cloned().collect();
         names.sort();
         names
     })
