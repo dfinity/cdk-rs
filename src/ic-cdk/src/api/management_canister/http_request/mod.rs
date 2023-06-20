11bd67cb1b180a0597fda7d1ebb06df2281d2906
Linwei Shang <linwei.shang@dfinity.org>
2023-06-20 14:04:14 -0400

    http_request fee

--- src/ic-cdk/src/api/management_canister/http_request/mod.rs
+++ src/ic-cdk/src/api/management_canister/http_request/mod.rs
@@ -3,12 +3,31 @@
 use crate::api::call::{call_with_payment128, CallResult};
 use candid::Principal;
 #[cfg(feature = "transform-closure")]
-use slotmap::{DefaultKey, Key, SlotMap};
+use slotmap::{DefaultKey, Key, KeyData, SlotMap};
 #[cfg(feature = "transform-closure")]
 use std::cell::RefCell;
 
 mod types;
 pub use types::*;
+
+/// Make an HTTP request to a given URL and return the HTTP response, possibly after a transformation.
+///
+/// See [IC method `http_request`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-http_request).
+///
+/// This call requires cycles payment. The required cycles is a function of the request size and max_response_bytes.
+/// Check [Gas and cycles cost](https://internetcomputer.org/docs/current/developer-docs/gas-cost) for more details.
+pub async fn http_request(
+    arg: CanisterHttpRequestArgument,
+    cycles: u128,
+) -> CallResult<(HttpResponse,)> {
+    call_with_payment128(
+        Principal::management_canister(),
+        "http_request",
+        (arg,),
+        cycles,
+    )
+    .await
+}
 
 #[cfg(feature = "transform-closure")]
 thread_local! {
@@ -23,7 +42,6 @@ extern "C" fn http_transform() {
         call::{arg_data, reply},
         caller,
     };
-    use slotmap::KeyData;
     if caller() != Principal::management_canister() {
         crate::trap("This function is internal to ic-cdk and should not be called externally.");
     }
@@ -39,26 +57,6 @@ extern "C" fn http_transform() {
     reply((transformed,))
 }
 
-/// Make an HTTP request to a given URL and return the HTTP response, possibly after a transformation.
-///
-/// See [IC method `http_request`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-http_request).
-///
-/// This call requires cycles payment. The required cycles is a function of the request size and max_response_bytes.
-/// This method handles the cycles cost calculation under the hood which assuming the canister is on a 13-node Application Subnet.
-/// If the canister is on a 34-node Application Subnets, you may have to compute the cost by yourself and call [`http_request_with_cycles`] instead.
-///
-/// Check [this page](https://internetcomputer.org/docs/current/developer-docs/production/computation-and-storage-costs) for more details.
-pub async fn http_request(arg: CanisterHttpRequestArgument) -> CallResult<(HttpResponse,)> {
-    let cycles = http_request_required_cycles(&arg);
-    call_with_payment128(
-        Principal::management_canister(),
-        "http_request",
-        (arg,),
-        cycles,
-    )
-    .await
-}
-
 /// Make an HTTP request to a given URL and return the HTTP response, after a transformation.
 ///
 /// Do not set the `transform` field of `arg`. To use a Candid function, call [`http_request`] instead.
@@ -66,54 +64,10 @@ pub async fn http_request(arg: CanisterHttpRequestArgument) -> CallResult<(HttpR
 /// See [IC method `http_request`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-http_request).
 ///
 /// This call requires cycles payment. The required cycles is a function of the request size and max_response_bytes.
-/// This method handles the cycles cost calculation under the hood which assuming the canister is on a 13-node Application Subnet.
-/// If the canister is on a 34-node Application Subnets, you may have to compute the cost by yourself and call [`http_request_with_cycles_with`] instead.
-///
-/// Check [this page](https://internetcomputer.org/docs/current/developer-docs/production/computation-and-storage-costs) for more details.
+/// Check [Gas and cycles cost](https://internetcomputer.org/docs/current/developer-docs/gas-cost) for more details.
 #[cfg(feature = "transform-closure")]
 #[cfg_attr(docsrs, doc(cfg(feature = "transform-closure")))]
-pub async fn http_request_with(
-    arg: CanisterHttpRequestArgument,
-    transform_func: impl FnOnce(HttpResponse) -> HttpResponse + 'static,
-) -> CallResult<(HttpResponse,)> {
-    let cycles = http_request_required_cycles(&arg);
-    http_request_with_cycles_with(arg, cycles, transform_func).await
-}
-
-/// Make an HTTP request to a given URL and return the HTTP response, possibly after a transformation.
-///
-/// See [IC method `http_request`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-http_request).
-///
-/// This call requires cycles payment. The required cycles is a function of the request size and max_response_bytes.
-/// Check [this page](https://internetcomputer.org/docs/current/developer-docs/production/computation-and-storage-costs) for more details.
-///
-/// If the canister is on a 13-node Application Subnet, you can call [`http_request`] instead which handles cycles cost calculation under the hood.
-pub async fn http_request_with_cycles(
-    arg: CanisterHttpRequestArgument,
-    cycles: u128,
-) -> CallResult<(HttpResponse,)> {
-    call_with_payment128(
-        Principal::management_canister(),
-        "http_request",
-        (arg,),
-        cycles,
-    )
-    .await
-}
-
-/// Make an HTTP request to a given URL and return the HTTP response, after a transformation.
-///
-/// Do not set the `transform` field of `arg`. To use a Candid function, call [`http_request_with_cycles`] instead.
-///
-/// See [IC method `http_request`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-http_request).
-///
-/// This call requires cycles payment. The required cycles is a function of the request size and max_response_bytes.
-/// Check [this page](https://internetcomputer.org/docs/current/developer-docs/production/computation-and-storage-costs) for more details.
-///
-/// If the canister is on a 13-node Application Subnet, you can call [`http_request_with`] instead which handles cycles cost calculation under the hood.
-#[cfg(feature = "transform-closure")]
-#[cfg_attr(docsrs, doc(cfg(feature = "transform-closure")))]
-pub async fn http_request_with_cycles_with(
+pub async fn http_request_with_closure(
     arg: CanisterHttpRequestArgument,
     cycles: u128,
     transform_func: impl FnOnce(HttpResponse) -> HttpResponse + 'static,
@@ -142,49 +96,5 @@ pub async fn http_request_with_cycles_with(
         }),
         ..arg
     };
-    http_request_with_cycles(arg, cycles).await
-}
-
-fn http_request_required_cycles(arg: &CanisterHttpRequestArgument) -> u128 {
-    let max_response_bytes = match arg.max_response_bytes {
-        Some(ref n) => *n as u128,
-        None => 2 * 1024 * 1024u128, // default 2MiB
-    };
-    let arg_raw = candid::utils::encode_args((arg,)).expect("Failed to encode arguments.");
-    // The coefficients can be found in [this page](https://internetcomputer.org/docs/current/developer-docs/production/computation-and-storage-costs).
-    // 12 is "http_request".len().
-    400_000_000u128 + 100_000u128 * (arg_raw.len() as u128 + 12 + max_response_bytes)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn required_cycles_some_max() {
-        let url = "https://example.com".to_string();
-        let arg = CanisterHttpRequestArgument {
-            url,
-            max_response_bytes: Some(3000),
-            method: HttpMethod::GET,
-            headers: vec![],
-            body: None,
-            transform: None,
-        };
-        assert_eq!(http_request_required_cycles(&arg), 718500000u128);
-    }
-
-    #[test]
-    fn required_cycles_none_max() {
-        let url = "https://example.com".to_string();
-        let arg = CanisterHttpRequestArgument {
-            url,
-            max_response_bytes: None,
-            method: HttpMethod::GET,
-            headers: vec![],
-            body: None,
-            transform: None,
-        };
-        assert_eq!(http_request_required_cycles(&arg), 210132900000u128);
-    }
+    http_request(arg, cycles).await
 }
