2d86b228a7b3dfc582cd8255ad2a785586390f1d
Linwei Shang <linwei.shang@dfinity.org>
2023-06-20 10:46:18 -0400

    ecdsa fees

--- src/ic-cdk/src/api/management_canister/ecdsa/mod.rs
+++ src/ic-cdk/src/api/management_canister/ecdsa/mod.rs
@@ -1,6 +1,6 @@
 //! The ECDSA API.
 
-use crate::api::call::{call, CallResult};
+use crate::api::call::{call_with_payment128, CallResult};
 use candid::Principal;
 
 mod types;
@@ -9,15 +9,35 @@ pub use types::*;
 /// Return a SEC1 encoded ECDSA public key for the given canister using the given derivation path.
 ///
 /// See [IC method `ecdsa_public_key`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-ecdsa_public_key).
+/// 
+/// This API charges cycles. Please check [`Gas and cycles cost`](https://internetcomputer.org/docs/current/developer-docs/gas-cost).
 pub async fn ecdsa_public_key(
     arg: EcdsaPublicKeyArgument,
+    cycles: u128,
 ) -> CallResult<(EcdsaPublicKeyResponse,)> {
-    call(Principal::management_canister(), "ecdsa_public_key", (arg,)).await
+    call_with_payment128(
+        Principal::management_canister(),
+        "ecdsa_public_key",
+        (arg,),
+        cycles,
+    )
+    .await
 }
 
 /// Return a new ECDSA signature of the given message_hash that can be separately verified against a derived ECDSA public key.
 ///
 /// See [IC method `sign_with_ecdsa`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-sign_with_ecdsa).
-pub async fn sign_with_ecdsa(arg: SignWithEcdsaArgument) -> CallResult<(SignWithEcdsaResponse,)> {
-    call(Principal::management_canister(), "sign_with_ecdsa", (arg,)).await
+/// 
+/// This API charges cycles. Please check [`Gas and cycles cost`](https://internetcomputer.org/docs/current/developer-docs/gas-cost).
+pub async fn sign_with_ecdsa(
+    arg: SignWithEcdsaArgument,
+    cycles: u128,
+) -> CallResult<(SignWithEcdsaResponse,)> {
+    call_with_payment128(
+        Principal::management_canister(),
+        "sign_with_ecdsa",
+        (arg,),
+        cycles,
+    )
+    .await
 }
