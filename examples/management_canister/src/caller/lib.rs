5a76a73ceef79f7e582d7b5e477752f40a066e24
Alexandra Zapuc <alexandra.zapuc@dfinity.org>
2023-07-04 14:51:28 +0000

    Update example http_request_required_cycles

--- examples/management_canister/src/caller/lib.rs
+++ examples/management_canister/src/caller/lib.rs
@@ -88,9 +88,10 @@ mod http_request {
         };
         let arg_raw = ic_cdk::export::candid::utils::encode_args((arg,))
             .expect("Failed to encode arguments.");
-        400_000_000u128 / 13
-            + 100_000u128 / 13
-                * (arg_raw.len() as u128 + "http_request".len() as u128 + max_response_bytes)
+        (3_000_000u128 + 60_000u128 * 13
+            + (arg_raw.len() as u128 + "http_request".len() as u128) * 400
+            + max_response_bytes * 800)
+            * 13
     }
 
     #[update]
