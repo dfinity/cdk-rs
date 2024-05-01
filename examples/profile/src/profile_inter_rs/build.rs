716b9cff679c6a452f52357ba0bdbcb10253f630
Linwei Shang <linwei.shang@dfinity.org>
2024-05-01 10:18:29 -0400

    verify that chain calls work

--- examples/profile/src/profile_inter_rs/build.rs
+++ examples/profile/src/profile_inter_rs/build.rs
@@ -1,6 +1,7 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     ic_cdk_bindgen::Builder::new("profile_rs")
+        .candid_path("../profile_rs/profile.did")
         .generate_consumer()
         .unwrap();
 }
