2ded651b8f38239a05e333eb49b061f2fdf1a669
Linwei Shang <linwei.shang@dfinity.org>
2024-09-16 15:30:37 -0400

    fmt

--- src/ic-cdk/src/api/management_canister/schnorr/mod.rs
+++ src/ic-cdk/src/api/management_canister/schnorr/mod.rs
@@ -14,7 +14,12 @@ const SIGN_WITH_SCHNORR_FEE: u128 = 26_153_846_153;
 pub async fn schnorr_public_key(
     arg: SchnorrPublicKeyArgument,
 ) -> CallResult<(SchnorrPublicKeyResponse,)> {
-    call(Principal::management_canister(), "schnorr_public_key", (arg,)).await
+    call(
+        Principal::management_canister(),
+        "schnorr_public_key",
+        (arg,),
+    )
+    .await
 }
 
 /// Return a new Schnorr signature of the given message that can be separately verified against a derived Schnorr public key.
@@ -24,7 +29,9 @@ pub async fn schnorr_public_key(
 /// This call requires cycles payment.
 /// This method handles the cycles cost under the hood.
 /// Check [Threshold signatures](https://internetcomputer.org/docs/current/references/t-sigs-how-it-works) for more details.
-pub async fn sign_with_schnorr(arg: SignWithSchnorrArgument) -> CallResult<(SignWithSchnorrResponse,)> {
+pub async fn sign_with_schnorr(
+    arg: SignWithSchnorrArgument,
+) -> CallResult<(SignWithSchnorrResponse,)> {
     call_with_payment128(
         Principal::management_canister(),
         "sign_with_schnorr",
