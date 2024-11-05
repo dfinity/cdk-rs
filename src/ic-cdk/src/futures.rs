6692c20f3d62b29837c47a91e2482a95f8bf85bb
Adam Spofford <adam.spofford@dfinity.org>
2024-11-05 09:13:08 -0800

    Switch to `Wake`

--- src/ic-cdk/src/futures.rs
+++ src/ic-cdk/src/futures.rs
@@ -3,7 +3,8 @@ use std::future::Future;
 use std::mem;
 use std::pin::Pin;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
+use std::sync::Arc;
+use std::task::{Context, Poll, Wake, Waker};
 
 use slotmap::{new_key_type, SlotMap};
 
@@ -17,8 +18,8 @@ pub fn spawn<F: 'static + Future<Output = ()>>(future: F) {
     let task = Task {
         future: pinned_future,
     };
-    let task_id = TASKS.with(|tasks| tasks.borrow_mut().insert(task));
-    let waker = TaskWaker { task_id };
+    let task_id = TASKS.with_borrow_mut(|tasks| tasks.insert(task));
+    let waker = Arc::new(TaskWaker { task_id });
     waker.wake();
 }
 
@@ -47,18 +48,18 @@ impl Default for Task {
 /// Waker implementation for executing futures produced by `call`/`call_raw`/etc.
 ///
 /// *Almost* a do-nothing executor, i.e. wake directly calls poll with no scheduler, except it attempts to clean up tasks
-/// whose execution has trapped - see `call::is_recovering_from_trap``.
+/// whose execution has trapped - see `call::is_recovering_from_trap`.
 #[derive(Clone)]
 struct TaskWaker {
     task_id: TaskId,
 }
 
-impl TaskWaker {
-    fn wake(&self) {
+impl Wake for TaskWaker {
+    fn wake(self: Arc<Self>) {
         if CLEANUP.load(Ordering::Relaxed) {
             // This task is recovering from a trap. We cancel it to run destructors.
-            TASKS.with(|tasks| {
-                tasks.borrow_mut().remove(self.task_id);
+            TASKS.with_borrow_mut(|tasks| {
+                tasks.remove(self.task_id);
             })
         } else {
             // Temporarily remove the task from the table. We need to execute it while `TASKS` is not borrowed, because it may schedule more tasks.
@@ -78,73 +79,22 @@ impl TaskWaker {
                 crate::trap("Call already trapped");
                 // This also should not happen because the CallFuture handles this itself. But FuturesUnordered introduces some chaos.
             };
-            let waker = self.clone().into_waker();
+            let waker = Waker::from(self.clone());
             let poll = task.future.as_mut().poll(&mut Context::from_waker(&waker));
             match poll {
                 Poll::Pending => {
                     // more to do, put the task back in the table
-                    TASKS.with(|tasks| {
-                        if let Some(t) = tasks.borrow_mut().get_mut(self.task_id) {
+                    TASKS.with_borrow_mut(|tasks| {
+                        if let Some(t) = tasks.get_mut(self.task_id) {
                             *t = task;
                         }
                     });
                 }
                 Poll::Ready(()) => {
                     // task complete, remove its entry from the table fully
-                    TASKS.with(|tasks| tasks.borrow_mut().remove(self.task_id));
+                    TASKS.with_borrow_mut(|tasks| tasks.remove(self.task_id));
                 }
             }
         }
     }
-
-    fn into_waker(self) -> Waker {
-        let raw_waker = raw_waker(self);
-        // SAFETY: raw_waker correctly implements RawWakerVTable's contract.
-        unsafe { Waker::from_raw(raw_waker) }
-    }
-}
-
-// Simple waker vtable implementation entirely unrelated to TaskWaker. Would be generic over a SimpleWaker trait
-// for unsafe separation's sake if statics could be generic, but they can't and you need a static for the vtable.
-
-static WAKER_VTABLE: RawWakerVTable = RawWakerVTable::new(clone, wake, wake_by_ref, drop);
-
-/// Produces a RawWaker from a pointer representing a Box<TaskWaker>.
-fn raw_waker(waker: TaskWaker) -> RawWaker {
-    RawWaker::new(
-        Box::into_raw(Box::new(waker)).cast::<()>().cast_const(),
-        &WAKER_VTABLE,
-    )
-}
-
-/// # Safety
-/// Must only be called from the RawWaker produced by raw_waker.
-unsafe fn clone(ptr: *const ()) -> RawWaker {
-    // SAFETY: RawWaker's contract says `ptr` is the data-pointer from raw_waker, i.e. Box<TaskWaker>, semantically borrowed immutably.
-    let waker = unsafe { ptr.cast::<TaskWaker>().as_ref().unwrap() };
-    let cloned = waker.clone();
-    raw_waker(cloned)
-}
-
-/// # Safety
-/// Must only be called from the RawWaker produced by raw_waker.
-unsafe fn wake(ptr: *const ()) {
-    // SAFETY: RawWaker's contract says `ptr` is the data-pointer from raw_waker, i.e. Box<TaskWaker>, semantically owned.
-    let waker = unsafe { Box::from_raw(ptr.cast::<TaskWaker>().cast_mut()) };
-    waker.wake();
-}
-
-/// # Safety
-/// Must only be called from the RawWaker produced by raw_waker.
-unsafe fn wake_by_ref(ptr: *const ()) {
-    // SAFETY: RawWaker's contract says `ptr` is the data-pointer from raw_waker, i.e. Box<TaskWaker>, semantically borrowed immutably.
-    let waker = unsafe { ptr.cast::<TaskWaker>().as_ref().unwrap() };
-    waker.wake();
-}
-
-/// # Safety
-/// Must only be called from the RawWaker produced by raw_waker.
-unsafe fn drop(ptr: *const ()) {
-    // SAFETY: RawWaker's contract says `ptr` is the data-pointer from raw_waker, i.e. Box<TaskWaker>, semantically owned.
-    let _waker = unsafe { Box::from_raw(ptr.cast::<TaskWaker>().cast_mut()) };
 }
