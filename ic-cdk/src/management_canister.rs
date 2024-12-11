6b4eab620214534c8a8c1146b6adfcc6e6622383
Linwei Shang <linwei.shang@dfinity.org>
2024-12-11 13:41:55 -0500

    add management canister mod at root level

---
+++ ic-cdk/src/management_canister.rs
@@ -0,0 +1,10 @@
+//! Functions and types for calling [the IC management canister][1].
+//!
+//! This module is a direct translation from its Candid interface description.
+//!
+//! The functions and types defined in this module serves these purposes:
+//! * Make it easy to construct correct request data.
+//! * Handle the response ergonomically.
+//! * For those calls require cycles payments, the cycles amount is an explicit argument.
+//!
+//! [1]: https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-management-canister
