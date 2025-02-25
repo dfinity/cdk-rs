48be9f17c91cf73effd37d40ea0b44cada4e1db2
Linwei Shang <linwei.shang@dfinity.org>
2025-02-25 10:22:57 -0500

    doc: bitcoin_canister mod with a placeholder file

---
+++ ic-cdk/src/bitcoin_canister.rs
@@ -0,0 +1,10 @@
+//! This module provides functionality for making inter-canister calls to the [Bitcoin Canisters][1].
+//!
+//! The Bitcoin Canisters allow for interactions with the Bitcoin network from within the Internet Computer.
+//! This module includes functions and types that facilitate these interactions, adhering to the
+//! [Bitcoin Canisters Interface Specification][2].
+//!
+//! [1]: https://github.com/dfinity/bitcoin-canister
+//! [2]: https://github.com/dfinity/bitcoin-canister/blob/master/INTERFACE_SPECIFICATION.md
+
+// TODO: Implementation of the Bitcoin Canister API
