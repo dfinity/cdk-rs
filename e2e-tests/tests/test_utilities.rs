eeb34f8e0a49b9e6e670910e2a0c2dd80c5228a1
Linwei Shang <linwei.shang@dfinity.org>
2025-03-04 10:35:31 -0500

    cache pocket-ic server binary

--- e2e-tests/tests/test_utilities.rs
+++ e2e-tests/tests/test_utilities.rs
@@ -1,8 +1,10 @@
 use candid::utils::{ArgumentDecoder, ArgumentEncoder};
 use candid::Principal;
 use cargo_metadata::MetadataCommand;
+use flate2::read::GzDecoder;
 use pocket_ic::common::rest::RawEffectivePrincipal;
 use pocket_ic::{call_candid, PocketIc, PocketIcBuilder, RejectResponse};
+use std::io::Read;
 use std::path::PathBuf;
 use std::process::Command;
 use std::sync::Once;
@@ -73,14 +75,6 @@ pub fn cargo_build_canister(bin_name: &str) -> Vec<u8> {
     })
 }
 
-pub fn pocket_ic() -> PocketIc {
-    PocketIcBuilder::new()
-        .with_application_subnet()
-        .with_nonmainnet_features(true)
-        .with_ii_subnet()
-        .build()
-}
-
 // The linter complains "function `update` is never used"
 // because not EVERY test uses this function.
 #[allow(dead_code)]
@@ -95,4 +89,83 @@ where
     Output: for<'a> ArgumentDecoder<'a>,
 {
     call_candid(env, canister_id, RawEffectivePrincipal::None, method, input)
+}
+
+/// Instantiates a PocketIc instance for e2e tests.
+///
+/// The PocketIc server binary is cached for reuse.
+pub fn pocket_ic() -> PocketIc {
+    let pocket_ic_server = cache_pocket_ic_server();
+    PocketIcBuilder::new()
+        .with_server_binary(pocket_ic_server)
+        .with_application_subnet()
+        .with_nonmainnet_features(true)
+        .with_ii_subnet()
+        .build()
+}
+
+fn cache_pocket_ic_server() -> PathBuf {
+    let dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let cargo_toml_path = dir.join("Cargo.toml");
+    let metadata = MetadataCommand::new()
+        .manifest_path(&cargo_toml_path)
+        .exec()
+        .expect("failed to run cargo metadata");
+    let e2e_tests_package = metadata
+        .packages
+        .iter()
+        .find(|m| m.name == "ic-cdk-e2e-tests")
+        .expect("ic-cdk-e2e-tests not found in Cargo.toml");
+    let pocket_ic_tag = e2e_tests_package
+        .dependencies
+        .iter()
+        .find(|d| d.name == "pocket-ic")
+        .expect("pocket-ic not found in Cargo.toml")
+        .source
+        .as_ref()
+        .expect("pocket-ic source not found in Cargo.toml")
+        .split_once("tag=")
+        .expect("`tag=` not found in pocket-ic source")
+        .1;
+    let target_dir = metadata.target_directory;
+    let artifact_dir = target_dir.join("e2e-tests-artifacts");
+    std::fs::create_dir_all(&artifact_dir).expect("failed to create artifact directory");
+    let tag_file = artifact_dir.join("pocket-ic-tag");
+    let pocket_ic_server = artifact_dir.join("pocket-ic-server");
+    if let Ok(tag) = std::fs::read_to_string(&tag_file) {
+        if tag == pocket_ic_tag && pocket_ic_server.exists() {
+            return pocket_ic_server.into();
+        }
+    }
+    let uname_sys = match std::env::consts::OS {
+        "macos" => "darwin",
+        "linux" => "linux",
+        other => panic!("unsupported OS: {}", other),
+    };
+    let url = format!(
+        "https://github.com/dfinity/ic/releases/download/{pocket_ic_tag}/pocket-ic-x86_64-{uname_sys}.gz");
+    let gz_bytes = reqwest::blocking::get(url)
+        .expect("failed to download pocket-ic-server")
+        .bytes()
+        .expect("failed to get bytes of pocket-ic-server")
+        .to_vec();
+    let mut decoder = GzDecoder::new(&gz_bytes[..]);
+    let mut decompressed_data = Vec::new();
+    decoder
+        .read_to_end(&mut decompressed_data)
+        .expect("failed to decompress pocket-ic-server");
+    std::fs::write(&pocket_ic_server, decompressed_data).expect("failed to write pocket-ic-server");
+    std::fs::write(tag_file, pocket_ic_tag).expect("failed to write pocket-ic-tag");
+    pocket_ic_server.into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cache_pocket_ic_server() {
+        let pocket_ic_server = cache_pocket_ic_server();
+        println!("{:?}", pocket_ic_server);
+    }
 }
