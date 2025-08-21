0894e5db2493fb28bf7886f4952187366f5296fe
Adam Spofford <adam.spofford@dfinity.org>
2025-08-21 08:17:38 -0700

    pin nightly

--- e2e-tests/tests/test_utilities.rs
+++ e2e-tests/tests/test_utilities.rs
@@ -31,7 +31,7 @@ pub fn cargo_build_canister(bin_name: &str) -> Vec<u8> {
     let target = match std::env::var("WASM64") {
         Ok(_) => {
             cmd.args([
-                "+nightly",
+                "+nightly-2025-01-03",
                 "build",
                 "-Z",
                 "build-std=std,panic_abort",
