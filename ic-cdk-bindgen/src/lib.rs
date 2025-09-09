524c06d1a641f2d0535b464908c1d4962d793219
Linwei Shang <linwei.shang@dfinity.org>
2025-09-09 11:11:37 -0400

    apply changes in PR #487

--- ic-cdk-bindgen/src/lib.rs
+++ ic-cdk-bindgen/src/lib.rs
@@ -1,141 +1,191 @@
 use candid::Principal;
+use candid_parser::bindings::rust::{Config, ExternalConfig, emit_bindgen, output_handlebar};
+use candid_parser::configs::Configs;
 use candid_parser::pretty_check_file;
+
 use std::env;
 use std::fs;
 use std::io::Write;
 use std::path::PathBuf;
+use std::str::FromStr;
 
-mod code_generator;
+mod error;
 
-#[derive(Clone)]
-pub struct Config {
-    pub canister_name: String,
-    pub candid_path: PathBuf,
-    pub skip_existing_files: bool,
-    pub binding: code_generator::Config,
-}
+pub use error::IcCdkBindgenError;
 
-impl Config {
-    pub fn new(canister_name: &str) -> Self {
-        let (candid_path, canister_id) = resolve_candid_path_and_canister_id(canister_name);
-        let mut binding = code_generator::Config::new();
-        binding
-            // User will depend on candid crate directly
-            .set_candid_crate("candid".to_string())
-            .set_canister_id(canister_id)
-            .set_service_name(canister_name.to_string())
-            .set_target(code_generator::Target::CanisterCall);
+type Result<T> = std::result::Result<T, IcCdkBindgenError>;
 
-        Config {
-            canister_name: canister_name.to_string(),
-            candid_path,
-            skip_existing_files: false,
-            binding,
-        }
-    }
-}
-
-/// Resolve the candid path and canister id from environment variables.
-///
-/// The name and format of the environment variables are standardized:
-/// <https://github.com/dfinity/sdk/blob/master/docs/cli-reference/dfx-envars.md#canister_id_canistername>
-///
-/// We previously used environment variables like `CANISTER_CANDID_PATH_<canister_name>` without `to_uppercase`.
-/// That is deprecated. To keep backward compatibility, we also check for the old format.
-/// Just in case the user runs `ic-cdk-bindgen` outside `dfx`.
-/// If the old format is found, we print a warning to the user.
-/// dfx v0.13.0 only provides the old format, which can be used to check the warning logic.
-/// TODO: remove the support for the old format, in the next major release (v0.2) of `ic-cdk-bindgen`.
-fn resolve_candid_path_and_canister_id(canister_name: &str) -> (PathBuf, Principal) {
-    fn warning_deprecated_env(deprecated_name: &str, new_name: &str) {
-        println!(
-            "cargo:warning=The environment variable {deprecated_name} is deprecated. Please set {new_name} instead. Upgrading dfx may fix this issue."
-        );
-    }
-
-    let canister_name = canister_name.replace('-', "_");
-    let canister_name_upper = canister_name.to_uppercase();
-
-    let candid_path_var_name = format!("CANISTER_CANDID_PATH_{canister_name_upper}");
-    let candid_path_var_name_legacy = format!("CANISTER_CANDID_PATH_{canister_name}");
-    println!("cargo:rerun-if-env-changed={candid_path_var_name}");
-    println!("cargo:rerun-if-env-changed={candid_path_var_name_legacy}");
-
-    let candid_path_str = if let Ok(candid_path_str) = env::var(&candid_path_var_name) {
-        candid_path_str
-    } else if let Ok(candid_path_str) = env::var(&candid_path_var_name_legacy) {
-        warning_deprecated_env(&candid_path_var_name_legacy, &candid_path_var_name);
-        candid_path_str
-    } else {
-        panic!(
-            "Cannot find environment variable: {}",
-            &candid_path_var_name
-        );
-    };
-    let candid_path = PathBuf::from(candid_path_str);
-
-    let canister_id_var_name = format!("CANISTER_ID_{canister_name_upper}");
-    let canister_id_var_name_legacy = format!("CANISTER_ID_{canister_name}");
-    println!("cargo:rerun-if-env-changed={canister_id_var_name}");
-    println!("cargo:rerun-if-env-changed={canister_id_var_name_legacy}");
-    let canister_id_str = if let Ok(canister_id_str) = env::var(&canister_id_var_name) {
-        canister_id_str
-    } else if let Ok(canister_id_str) = env::var(&canister_id_var_name_legacy) {
-        warning_deprecated_env(&canister_id_var_name_legacy, &canister_id_var_name);
-        canister_id_str
-    } else {
-        panic!(
-            "Cannot find environment variable: {}",
-            &canister_id_var_name
-        );
-    };
-    let canister_id = Principal::from_text(&canister_id_str)
-        .unwrap_or_else(|_| panic!("Invalid principal: {}", &canister_id_str));
-
-    (candid_path, canister_id)
-}
-
-#[derive(Default)]
+#[derive(Debug, Clone)]
 pub struct Builder {
-    configs: Vec<Config>,
+    pub(crate) canister_name: String,
+    pub(crate) canister_id: Option<Principal>,
+    pub(crate) candid_path: Option<PathBuf>,
+    pub(crate) out_dir: Option<PathBuf>,
 }
 
+// Configurations.
 impl Builder {
-    pub fn new() -> Self {
-        Builder {
-            configs: Vec::new(),
+    /// Create a new builder with the given canister name.
+    pub fn new<S>(canister_name: S) -> Self
+    where
+        S: Into<String>,
+    {
+        Self {
+            canister_name: canister_name.into(),
+            canister_id: None,
+            candid_path: None,
+            out_dir: None,
         }
     }
-    pub fn add(&mut self, config: Config) -> &mut Self {
-        self.configs.push(config);
+
+    /// Set the canister id.
+    ///
+    /// This is only needed for the consumer target.
+    ///
+    /// If not set, the canister id will be resolved from the environment variable `CANISTER_ID_<CANISTER_NAME>`.
+    pub fn canister_id<S>(&mut self, canister_id: S) -> &mut Self
+    where
+        S: Into<Principal>,
+    {
+        self.canister_id = Some(canister_id.into());
         self
     }
-    pub fn build(self, out_path: Option<PathBuf>) {
-        let out_path = out_path.unwrap_or_else(|| {
-            let manifest_dir =
-                PathBuf::from(env::var("CARGO_MANIFEST_DIR").expect("Cannot find manifest dir"));
-            manifest_dir.join("src").join("declarations")
-        });
-        fs::create_dir_all(&out_path).unwrap();
-        for conf in &self.configs {
-            let (env, actor) =
-                pretty_check_file(&conf.candid_path).expect("Cannot parse candid file");
-            let content = code_generator::compile(&conf.binding, &env, actor.as_ref());
-            let generated_path = out_path.join(format!("{}.rs", conf.canister_name));
-            if !(conf.skip_existing_files && generated_path.exists()) {
-                fs::write(generated_path, content).expect("Cannot store generated binding");
-            }
-        }
-        let mut module = fs::File::create(out_path.join("mod.rs")).unwrap();
-        module.write_all(b"#![allow(unused_imports)]\n").unwrap();
-        module
-            .write_all(b"#![allow(non_upper_case_globals)]\n")
-            .unwrap();
-        module.write_all(b"#![allow(non_snake_case)]\n").unwrap();
-        for conf in &self.configs {
-            module.write_all(b"#[rustfmt::skip]\n").unwrap(); // so that we get a better diff
-            let line = format!("pub mod {};\n", conf.canister_name);
-            module.write_all(line.as_bytes()).unwrap();
-        }
+
+    /// Set the path to the candid file.
+    ///
+    /// If not set, the path will be resolved from the environment variable `CANISTER_CANDID_PATH_<CANISTER_NAME>`.
+    pub fn candid_path<S>(&mut self, path: S) -> &mut Self
+    where
+        S: Into<PathBuf>,
+    {
+        self.candid_path = Some(path.into());
+        self
     }
+
+    /// Set the output directory.
+    ///
+    /// If not set, the output directory will be resolved from the environment variable `OUT_DIR` set by cargo.
+    ///
+    /// The generated files will be placed in a subdirectory of the output directory based on the target:
+    /// - `/consumer/` for the [consumer](Self::generate_consumer) target
+    /// - `/provider/` for the [provider](Self::generate_provider) target
+    /// - `/type/` for the [type](Self::generate_type) target
+    pub fn out_dir<S>(&mut self, path: S) -> &mut Self
+    where
+        S: Into<PathBuf>,
+    {
+        self.out_dir = Some(path.into());
+        self
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Target {
+    Consumer,
+    Provider,
+    Type,
+}
+
+// Code generation.
+impl Builder {
+    fn generate(&self, target: Target) -> Result<()> {
+        // 1. Parse the candid file and generate the Output (the struct for bindings)
+        let config = Config::new(Configs::from_str("").unwrap());
+        let candid_path = if let Some(p) = &self.candid_path {
+            p.clone()
+        } else {
+            candid_path_from_env(&self.canister_name)?
+        };
+
+        let (env, actor, prog) = pretty_check_file(&candid_path).expect("Cannot parse candid file");
+        let (output, unused) = emit_bindgen(&config, &env, &actor, &prog);
+        // TODO: handle unused.
+        assert!(unused.is_empty());
+
+        // 2. Generate the Rust bindings using the Handlebars template
+        let template = match target {
+            Target::Consumer => include_str!("templates/consumer.hbs"),
+            Target::Provider => include_str!("templates/provider.hbs"),
+            Target::Type => include_str!("templates/type.hbs"),
+        };
+        let mut external = ExternalConfig::default();
+        if target == Target::Consumer {
+            let canister_id = if let Some(p) = &self.canister_id {
+                *p
+            } else {
+                canister_id_from_env(&self.canister_name)?
+            };
+            external
+                .0
+                .insert("canister_id".to_string(), canister_id.to_string());
+        }
+        external
+            .0
+            .insert("service_name".to_string(), self.canister_name.to_string());
+        let content = output_handlebar(output, external, template);
+
+        // 3. Write the generated Rust bindings to the output directory
+        let out_dir = if let Some(p) = &self.out_dir {
+            p.clone()
+        } else {
+            out_dir_from_env()?
+        };
+        let sub_dir_str = match target {
+            Target::Consumer => "consumer",
+            Target::Provider => "provider",
+            Target::Type => "type",
+        };
+        let sub_dir = out_dir.join(sub_dir_str);
+        fs::create_dir_all(&sub_dir)?;
+
+        let generated_path = sub_dir.join(format!("{}.rs", &self.canister_name));
+        let mut file = fs::File::create(generated_path)?;
+        writeln!(file, "{content}")?;
+        Ok(())
+    }
+
+    /// Generate the Rust bindings for consumer (inter-canister calls).
+    pub fn generate_consumer(&self) -> Result<()> {
+        self.generate(Target::Consumer)
+    }
+
+    /// Generate the Rust bindings for provider (implement canister entry-points).
+    pub fn generate_provider(&self) -> Result<()> {
+        self.generate(Target::Provider)
+    }
+
+    /// Generate the Rust bindings for type only (types used in the candid file).
+    pub fn generate_type(&self) -> Result<()> {
+        self.generate(Target::Type)
+    }
+}
+
+// https://github.com/dfinity/sdk/blob/master/docs/cli-reference/dfx-envars.mdx#canister_candid_path_canistername
+fn candid_path_from_env(canister_name: &str) -> Result<PathBuf> {
+    let canister_name_upper = canister_name.replace('-', "_").to_uppercase();
+    let candid_path_var_name = format!("CANISTER_CANDID_PATH_{}", canister_name_upper);
+    let candid_path_str = var_from_env(&candid_path_var_name)?;
+    println!("cargo:rerun-if-env-changed={candid_path_var_name}");
+    Ok(PathBuf::from(candid_path_str))
+}
+
+// https://github.com/dfinity/sdk/blob/master/docs/cli-reference/dfx-envars.mdx#canister_id_canistername
+fn canister_id_from_env(canister_name: &str) -> Result<Principal> {
+    let canister_name_upper = canister_name.replace('-', "_").to_uppercase();
+    let canister_id_var_name = format!("CANISTER_ID_{}", canister_name_upper);
+    let canister_id_str = var_from_env(&canister_id_var_name)?;
+    println!("cargo:rerun-if-env-changed={canister_id_var_name}");
+    Ok(Principal::from_text(canister_id_str)?)
+}
+
+fn out_dir_from_env() -> Result<PathBuf> {
+    let out_dir_str = var_from_env("OUT_DIR")?;
+    Ok(PathBuf::from(out_dir_str))
+}
+
+fn var_from_env(var: &str) -> Result<String> {
+    env::var(var).map_err(|e| IcCdkBindgenError::EnvVarNotFound {
+        var: var.to_string(),
+        source: e,
+    })
 }
